de8fbbed1935bd0e3e1c5246cc3870edc5463257
Egor Kirpichev <egor.kirpichev@gmail.com>
2019-08-19 16:43:06 +0200

    fix: test timeout with mock function

--- packages/components/src/components/alert/alert.spec.ts
+++ packages/components/src/components/alert/alert.spec.ts
@@ -1,7 +1,8 @@
 import { newSpecPage } from '@stencil/core/testing';
-import { Alert } from './alert';
+import { Alert, defaultTimeout } from './alert';
 
 describe('Alert', () => {
+<<<<<<< HEAD
   let element;
   beforeEach(async () => {
     element = new Alert();
@@ -51,6 +52,77 @@ describe('Alert', () => {
   // 	expect(setTimeout).toHaveBeenCalledTimes(1);
   // 	expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), null);
   // });
+=======
+	let element;
+	beforeEach(async () => {
+		element = new Alert();
+		jest.useFakeTimers();
+	});
+
+	it('should match snapshot', async () => {
+		const page = await newSpecPage({
+			components: [Alert],
+			html: `<t-alert>Notifications</t-alert>`,
+		});
+		expect(page.root).toMatchSnapshot();
+	});
+
+	it('should compile', () => {
+		expect(element).toBeTruthy();
+	});
+
+	it('should close the alert', () => {
+		expect(element.opened).toBe(undefined);
+		element.onCloseAlert();
+		expect(element.opened).toBe(false);
+	});
+
+	it('should open the alert', () => {
+		expect(element.opened).toBe(undefined);
+		element.open();
+		expect(element.opened).toBe(true);
+	});
+
+	it('should open the alert without timeout', () => {
+
+	});
+
+	it('should closed the alert after default timeout', () => {
+		element.timeout=true;
+		element.onCloseAlertWithTimeout();
+
+		expect(setTimeout).toHaveBeenCalledTimes(1);
+		expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), defaultTimeout);
+	});
+
+	it('should closed the alert after set timeout', () => {
+		element.timeout=500;
+		element.onCloseAlertWithTimeout();
+
+		expect(setTimeout).toHaveBeenCalledTimes(1);
+		expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 500);
+	})
+
+	it('should have a default css class', () => {
+		expect(element.getCssClassMap()).toBe('alert');
+	})
+
+	it('should handle size css class', () => {
+		element.size = 'small'
+		expect(element.getCssClassMap()).toContain('alert--size-small');
+	})
+
+	it('should handle theme css class', () => {
+		element.theme = 'default'
+		expect(element.getCssClassMap()).toContain('alert--theme-default');
+	})
+
+	it('should handle variant css class', () => {
+		element.variant = 'primary'
+		expect(element.getCssClassMap()).toContain('alert--variant-primary');
+	})
+});
+>>>>>>> fix: test timeout with mock function
 
   it('should closed the alert after default timeout', () => {
     element.timeout = true;
