da6f666a37ba98efa4546f041dc8046bbe28d252
Maomao <maomao.zha@gmail.com>
2019-08-21 14:50:35 +0200

    feat: add basic tag markup

---
+++ packages/components/src/components/tag/tag.e2e.ts
@@ -0,0 +1,10 @@
+import { newE2EPage } from "@stencil/core/testing";
+
+describe("t-tag", () => {
+  it("renders", async () => {
+    const page = await newE2EPage();
+    await page.setContent("<t-tag>this is a tag</t-tag>");
+    const element = await page.find("t-tag");
+    expect(element).toHaveClass("hydrated");
+  });
+});
