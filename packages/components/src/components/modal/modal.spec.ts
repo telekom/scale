1d8d9789334c5c62da36a696b3898cff3ef896ad
Sylvia Gonschior <sylvia.gonschior@telekom.de>
2019-09-19 11:50:24 +0200

    add unit tests and documentation in modal

--- packages/components/src/components/modal/modal.spec.ts
+++ packages/components/src/components/modal/modal.spec.ts
@@ -18,6 +18,23 @@ describe('Modal', () => {
   it('should handle size css class', () => {
     element.size = 'small'
     expect(element.getCssClassMap()).toContain('modal--size-small');
-  })
+  });
+
+  it('should open the modal', () => {
+    expect(element.opened).toBe(false);
+    element.openModal();
+    expect(element.opened).toBe(true);
+  });
+
+  it('should close the modal', () => {
+    expect(element.opened).toBe(false);
+    element.onCloseModal();
+    expect(element.opened).toBe(false);
+  });
+
+  it('should not open the modal/ should not render, if the modal is already opened', () => {
+    element.opened = true;
+    expect(element.root).toBeFalsy();
+  });
 
 });
