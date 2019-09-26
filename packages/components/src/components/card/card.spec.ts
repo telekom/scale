e1cd5ee0e1aafc26ffb46d89f49281519147907a
Egor Kirpichev <egor.kirpichev@gmail.com>
2019-09-26 15:31:04 +0200

    test: card slots (#42)

--- packages/components/src/components/card/card.spec.ts
+++ packages/components/src/components/card/card.spec.ts
@@ -13,41 +13,67 @@ describe('Card', () => {
 			html: `<t-card>Label</t-card>`,
 		});
 		expect(page.root).toMatchSnapshot();
-    });
+	});
 
-    it('should match snapshot with image', async () => {
+	it('should match snapshot with header slot', async () => {
+		const page = await newSpecPage({
+			components: [Card],
+			html: `
+			<t-card>
+				<h3 slot="header">Header content</h3>
+				A title
+			</t-card>
+			`,
+		});
+		expect(page.root).toMatchSnapshot();
+	});
+
+	it('should match snapshot with footer slot', async () => {
+		const page = await newSpecPage({
+			components: [Card],
+			html: `
+			<t-card>
+				<h3 slot="footer">Footer content</h3>
+				A title
+			</t-card>
+			`,
+		});
+		expect(page.root).toMatchSnapshot();
+	});
+
+	it('should match snapshot with image', async () => {
 		const page = await newSpecPage({
 			components: [Card],
 			html: `<t-card image-top="http://placehold.it/400x300">A title</t-card>`,
 		});
 		expect(page.root).toMatchSnapshot();
-    });
+	});
 
 	it('should have a default css class', () => {
 		expect(element.getCssClassMap()).toBe('card');
 	})
 
-    it('should handle size css class', () => {
+	it('should handle size css class', () => {
 		element.size = 'small'
 		expect(element.getCssClassMap()).toContain('card--size-small');
-    })
-    
-    it('should handle theme css class', () => {
+	})
+
+	it('should handle theme css class', () => {
 		element.theme = 'default';
 		expect(element.getCssClassMap()).toContain('card--theme-default');
-    })
-    
-    it('should handle variant css class', () => {
+	})
+
+	it('should handle variant css class', () => {
 		element.variant = 'primary';
 		expect(element.getCssClassMap()).toContain('card--variant-primary');
-    })
-    
-    it('should handle disabled css class', () => {
-        element.disabled = true;
+	})
+
+	it('should handle disabled css class', () => {
+		element.disabled = true;
 		expect(element.getCssClassMap()).toContain('card--disabled');
-    })
-    
-    it('should handle deselected css class', () => {
+	})
+
+	it('should handle deselected css class', () => {
 		element.deselected = true;
 		expect(element.getCssClassMap()).toContain('card--deselected');
 	})
