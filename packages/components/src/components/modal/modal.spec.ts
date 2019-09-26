3ca0a378c64e0631555b8c4878ff1b969ce38098
Sylvia Gonschior <sylvia.gonschior@telekom.de>
2019-09-26 09:28:09 +0200

    update modal unit test

--- packages/components/src/components/modal/modal.spec.ts
+++ packages/components/src/components/modal/modal.spec.ts
@@ -1,40 +1,60 @@
-import { newSpecPage } from '@stencil/core/testing';
-import { Modal } from './modal';
+import { newSpecPage } from "@stencil/core/testing";
+import { Modal } from "./modal";
 
-describe('Modal', () => {
+describe("Modal", () => {
   let element;
   beforeEach(async () => {
-    element = new Modal()
+    element = new Modal();
   });
 
-  it('should match snapshot', async () => {
+  const components = [Modal];
+
+  it("should match snapshot", async () => {
     const page = await newSpecPage({
       components: [Modal],
-      html: `<t-modal>Label</t-modal>`,
+      html: `<t-modal>Label</t-modal>`
     });
     expect(page.root).toMatchSnapshot();
   });
 
-  it('should handle size css class', () => {
-    element.size = 'small'
+  it("should match snapshot when opened", async () => {
+    const page = await newSpecPage({
+      components,
+      html: `<t-modal opened=true >Label</t-modal>`
+    });
+    expect(page.root.shadowRoot).toBeTruthy();
+    expect(page.root).toMatchSnapshot();
+  });
+
+  it("should handle size css class", () => {
+    element.size = 'small';
     expect(element.getCssClassMap()).toContain('modal--size-small');
   });
 
-  it('should open the modal', () => {
+  it("should handle theme css class", () => {
+    element.theme = 'default';
+    expect(element.getCssClassMap()).toContain('modal--theme-default');
+  });
+
+  it("should handle variant css class", () => {
+    element.variant = 'primary';
+    expect(element.getCssClassMap()).toContain('modal--variant-primary');
+  });
+
+  it("should open the modal", () => {
     expect(element.opened).toBe(false);
     element.openModal();
     expect(element.opened).toBe(true);
   });
 
-  it('should close the modal', () => {
+  it("should close the modal", () => {
     expect(element.opened).toBe(false);
     element.onCloseModal();
     expect(element.opened).toBe(false);
   });
 
-  it('should not open the modal/ should not render, if the modal is already opened', () => {
+  it("should not open the modal/ should not render, if the modal is already opened", () => {
     element.opened = true;
     expect(element.root).toBeFalsy();
   });
-
 });
