e60db772c7179abe8c752d3a25d6b894147e5f20
Sylvia Gonschior <sylvia.gonschior@telekom.de>
2019-10-21 15:29:55 +0200

    update the closeModal function and added onCloseModal function for the cancel button in the modal header and buttons

--- packages/components/src/components/modal/modal.tsx
+++ packages/components/src/components/modal/modal.tsx
@@ -16,11 +16,11 @@ export class Modal {
   @Prop() theme?: string = "";
   /** (optional) Modal variant */
   @Prop() variant?: string = "";
-  @Prop() opened?: boolean = false;
+  @Prop({ reflectToAttr: true }) opened?: boolean = false;
   @Prop() close?: string = "x";
 
   private hasSlotHeader: boolean;
-  private hasActions: boolean;
+  private hasSlotActions: boolean;
 
   private getCssClassMap(): CssClassMap {
     return classNames(
@@ -34,14 +34,21 @@ export class Modal {
   async openModal() {
     this.opened = true;
   }
+
+  closeModal = () => {
+    this.opened = false;
+  };
+
   @Method()
   async onCloseModal() {
     this.opened = false;
-  }
+  };
 
   componentWillLoad() {
     this.hasSlotHeader = !!this.hostElement.querySelector('[slot="header"]');
-    this.hasActions = !!this.hostElement.querySelector('[slot="modal-actions"]');
+    this.hasSlotActions = !!this.hostElement.querySelector(
+      '[slot="modal-actions"]'
+    );
   }
 
   render() {
@@ -51,13 +58,13 @@ export class Modal {
 
     return (
       <div class={this.getCssClassMap()}>
-        <div class="modal__backdrop" onClick={() => this.onCloseModal}></div>
+        <div class="modal__backdrop" onClick={this.closeModal}></div>
 
         <div class="modal">
           {this.hasSlotHeader && (
             <div class="modal__header">
               <slot name="header" />
-              <a class="modal__close" onClick={this.onCloseModal}>
+              <a class="modal__close" onClick={this.closeModal}>
                 {this.close}
               </a>
             </div>
@@ -66,13 +73,13 @@ export class Modal {
           <div class="modal__body">
             <slot />
             {!this.hasSlotHeader && (
-              <a class="modal__close" onClick={this.onCloseModal}>
+              <a class="modal__close" onClick={this.closeModal}>
                 {this.close}
               </a>
             )}
           </div>
 
-          {this.hasActions && (
+          {this.hasSlotActions && (
             <div class="modal__actions">
               <slot name="modal-actions" />
             </div>
