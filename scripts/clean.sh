437aa99fabb918835ca3f7ebeafb0d2ce3d5ac84
Egor Kirpichev <egor.kirpichev@gmail.com>
2019-11-21 11:03:17 +0100

    add docs setup

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -3,6 +3,5 @@ rimraf ./packages/*/node_modules;
 rimraf ./packages/*/build;
 rimraf ./packages/*/dist;
 rimraf ./packages/*/tsconfig.tsbuildinfo;
-rimraf ./packages/boilerplate-next/.next;
 rimraf ./yarn.lock;
 rimraf ./node_modules;
