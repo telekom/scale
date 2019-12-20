dcb73ba95b7911a5d5f65ad2f1b35aa657c4691c
Maomao <maomao.zha@gmail.com>
2019-12-20 11:01:29 +0100

    feat: add star svg and functions to hanlde select

--- packages/components/src/components/star-rating/star-rating.tsx
+++ packages/components/src/components/star-rating/star-rating.tsx
@@ -1,7 +1,7 @@
-import { Component, h, Prop } from '@stencil/core';
+import { Component, h, Prop, State } from '@stencil/core';
 import star from './star.svg';
-/* import { CssClassMap } from '../../utils/utils';
-import classNames from 'classnames'; */
+import { CssClassMap } from '../../utils/utils';
+import classNames from 'classnames';
 
 @Component({
   tag: 't-star-rating',
@@ -9,17 +9,58 @@ import classNames from 'classnames'; */
   shadow: true,
 })
 export class StarRating {
+  @Prop() public range = [1, 2, 3, 4, 5];
   @Prop() public disabled?: boolean = false;
+
+  @State() private selectedValue = null;
+  @State() private preSelectValue = 0;
+  @State() private selected = false;
+
+  public handleMouseEnter = val => {
+    this.preSelectValue = val;
+  };
+
+  public handleMouseLeave = val => {
+    if (this.selectedValue) {
+      this.preSelectValue = val;
+    } else {
+      this.preSelectValue = 0;
+    }
+  };
+
+  public handleClick = val => {
+    if (this.selectedValue !== val) {
+      this.selectedValue = val;
+      this.preSelectValue = val;
+    } else {
+      this.selectedValue = null;
+      this.preSelectValue = 0;
+    }
+  };
 
   public render() {
     return (
-      <div>
-        <img src={star} alt="star rating" />
+      <div class="star-rating">
+        {this.range.map(val => {
+          this.selected = val <= this.preSelectValue;
+          return (
+            <div
+              class={this.getCssClassMap()}
+              onMouseEnter={() => this.handleMouseEnter(val)}
+              onMouseLeave={() => this.handleMouseLeave(val)}
+              onClick={() => this.handleClick(val)}
+              innerHTML={star}
+            />
+          );
+        })}
       </div>
     );
   }
 
-  /*   private getCssClassMap(): CssClassMap {
-    return classNames('star-rating', this.disabled && `star--rating--disabled`);
-  } */
+  private getCssClassMap(): CssClassMap {
+    return classNames(
+      'star-rating--svg-container',
+      this.selected && `star--selected`
+    );
+  }
 }
--- packages/components/stencil.config.ts
+++ packages/components/stencil.config.ts
@@ -1,18 +1,20 @@
 import { Config } from '@stencil/core';
+import { inlineSvg } from 'stencil-inline-svg';
 
 export const config: Config = {
   namespace: 'telements-components',
   outputTargets: [
     {
       type: 'dist',
-      esmLoaderPath: '../loader'
+      esmLoaderPath: '../loader',
     },
     {
-      type: 'docs-readme'
+      type: 'docs-readme',
     },
     {
       type: 'www',
-      serviceWorker: null // disable service workers
-    }
-  ]
+      serviceWorker: null, // disable service workers
+    },
+  ],
+  plugins: [inlineSvg()],
 };
