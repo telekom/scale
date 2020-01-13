c66e2fd166a0cc47092b2af326c064a6e4a6266d
Maomao <maomao.zha@gmail.com>
2020-01-13 13:23:18 +0100

    fix: add path value  in file svgTransform.js

--- test/__mocks__/svgTransform.js
+++ test/__mocks__/svgTransform.js
@@ -1,9 +1,11 @@
+const path = require("path");
+
 module.exports = {
-    process(src, filename) {
-        return `module.exports = ${JSON.stringify(path.basename(filename))}`;
-    },
-    getCacheKey() {
-        // The output is always the same.
-        return 'svgTransform';
-    },
+  process(src, filename) {
+    return `module.exports = ${JSON.stringify(path.basename(filename))}`;
+  },
+  getCacheKey() {
+    // The output is always the same.
+    return "svgTransform";
+  }
 };
