10fe013d0305bbe4eb8983315454ee362b4df61a
Egor Kirpichev <egor.kirpichev@gmail.com>
2020-02-27 20:27:39 +0100

    fix: add build output

---
+++ packages/react-wrapper/build/index.d.ts
@@ -0,0 +1,2 @@
+import { Alert, Badge, Button, Card, Carousel, Divider, Icon, Input, InputError, InputGroup, InputLabel, Link, Modal, ProgressBar, Slider, Switch, Tag, Text, Toast } from './components/Components';
+export { Alert, Badge, Button, Card, Carousel, Divider, Icon, Input, InputError, InputGroup, InputLabel, Link, Modal, ProgressBar, Slider, Switch, Tag, Text, Toast };
