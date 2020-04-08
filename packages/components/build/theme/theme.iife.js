3dc0913fdb54ddaf25e5a82ab4bfd4a9739a052a
Egor <egor.kirpichev@gmail.com>
2020-04-08 14:22:37 +0200

    add text and default theming tokens

--- packages/components/build/theme/theme.iife.js
+++ packages/components/build/theme/theme.iife.js
@@ -2402,61 +2402,63 @@ var scale = (function (exports) {
           fontWeightRegular: 400,
           fontWeightMedium: 500,
           fontWeightBold: 700,
-          h1: {
-              fontWeight: 300,
-              fontSize: '6rem',
-              lineHeight: 1.167,
-              letterSpacing: '-0.01562em',
-          },
-          h2: {
-              fontWeight: 300,
-              fontSize: '3.75rem',
-              lineHeight: 1.2,
-              letterSpacing: '-0.00833em',
-          },
-          h3: {
-              fontWeight: 400,
-              fontSize: '3rem',
-              lineHeight: 1.167,
-              letterSpacing: '0em',
-          },
-          h4: {
-              fontWeight: 400,
-              fontSize: '2.125rem',
-              lineHeight: 1.235,
-              letterSpacing: '0.00735em',
-          },
-          h5: {
-              fontWeight: 400,
-              fontSize: '1.5rem',
-              lineHeight: 1.334,
-              letterSpacing: '0em',
-          },
-          h6: {
-              fontWeight: 500,
-              fontSize: '1.25rem',
-              lineHeight: 1.6,
-              letterSpacing: '0.0075em',
-          },
-          body: {
-              fontWeight: 400,
-              fontSize: '1rem',
-              lineHeight: 1.5,
-              letterSpacing: '0.00938em',
-          },
-          button: {
-              fontWeight: 500,
-              fontSize: '0.875rem',
-              lineHeight: 1.75,
-              letterSpacing: '0.02857em',
-              textTransform: 'none',
-          },
-          caption: {
-              fontWeight: 400,
-              fontSize: '0.75rem',
-              lineHeight: 1.66,
-              letterSpacing: '0.03333em',
-          },
+          variants: {
+              h1: {
+                  fontWeight: 500,
+                  fontSize: '2.5rem',
+                  lineHeight: 1.2,
+                  letterSpacing: '-0.01562em'
+              },
+              h2: {
+                  fontWeight: 500,
+                  fontSize: '2rem',
+                  lineHeight: 1.2,
+                  letterSpacing: '-0.00833em',
+              },
+              h3: {
+                  fontWeight: 500,
+                  fontSize: '1.75rem',
+                  lineHeight: 1.2,
+                  letterSpacing: '0em'
+              },
+              h4: {
+                  fontWeight: 500,
+                  fontSize: '1.5rem',
+                  lineHeight: 1.2,
+                  letterSpacing: '0.00735em'
+              },
+              h5: {
+                  fontWeight: 500,
+                  fontSize: '1.25rem',
+                  lineHeight: 1.2,
+                  letterSpacing: '0em',
+              },
+              h6: {
+                  fontWeight: 500,
+                  fontSize: '1rem',
+                  lineHeight: 1.2,
+                  letterSpacing: '0.0075em',
+              },
+              body: {
+                  fontWeight: 400,
+                  fontSize: '1rem',
+                  lineHeight: 1.5,
+                  letterSpacing: '0.00938em'
+              },
+              button: {
+                  fontWeight: 500,
+                  fontSize: '0.875rem',
+                  lineHeight: 1.75,
+                  letterSpacing: '0.02857em',
+                  textTransform: 'none',
+              },
+              caption: {
+                  fontWeight: 400,
+                  fontSize: '0.75rem',
+                  lineHeight: 1.66,
+                  letterSpacing: '0.03333em',
+              },
+          }
       },
       shape: {
           borderRadius: 4,
