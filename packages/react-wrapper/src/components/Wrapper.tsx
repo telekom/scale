4c7320ea3db79bc8f2722c08f6e945eacd978145
Maomao <maomao.zha@gmail.com>
2020-04-20 13:53:22 +0200

    fix: convert to class component of react wrapper

--- packages/react-wrapper/src/components/Wrapper.tsx
+++ packages/react-wrapper/src/components/Wrapper.tsx
@@ -1,41 +1,54 @@
-import React, { useRef, useEffect } from 'react'
+import React,  { Component, createRef } from "react";
 
-const kebabCase = require('lodash').kebabCase
+const kebabCase = require("lodash").kebabCase;
 
-const WebComponentWrapper = (props: any) => {
-  const { events, component, styles, children, ...forwardedProps } = props
-  const eventNames: string[] | null = events ? Object.keys(events) : null
-  const Component = component
-  const ref = useRef<any>(null)
-  var convertedProps = {}
-  Object.keys(forwardedProps).forEach(function (prop) {
-    if (prop === 'className') {
-      convertedProps['custom-class'] = forwardedProps[prop]
-    } else if (!prop.startsWith('on')) {
-      convertedProps[kebabCase(prop)] = forwardedProps[prop]
-    } else {
-      convertedProps[prop] = forwardedProps[prop]
-    }
-  })
-  useEffect(() => {
-    if (ref.current) {
-      if (styles) {
-        ref.current.styles = styles
+class WebComponentWrapper extends Component {
+  constructor(props) {
+    super(props);
+    this.componentRef = createRef();
+  }
+
+  render() {
+    const {
+      events,
+      component,
+      styles,
+      children,
+      ...forwardedProps
+    } = this.props;
+
+    const eventNames: string[] | null = events ? Object.keys(events) : null;
+    const Component = component;
+    var convertedProps = {};
+    Object.keys(forwardedProps).forEach(function(prop) {
+      if (prop === "className") {
+        convertedProps["custom-class"] = forwardedProps[prop];
+      } else if (!prop.startsWith("on")) {
+        convertedProps[kebabCase(prop)] = forwardedProps[prop];
+      } else {
+        convertedProps[prop] = forwardedProps[prop];
       }
-      if (eventNames) {
-        eventNames.map((eventName: string) =>
-          ref.current.addEventListener(events[eventName], props[eventName])
-        )
+    });
+
+    componentDidMount() {
+      if (this.componentRef.current) {
+        if (styles) {
+          this.componentRef.current.styles = styles;
+        }
+        if (eventNames) {
+          eventNames.map((eventName: string) =>
+            this.componentRef.current.addEventListener(events[eventName], this.props[eventName])
+          );
+        }
       }
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [ref, styles])
 
-  return (
-    <Component ref={ref} {...convertedProps}>
-      {children}
-    </Component>
-  )
+    return (
+      <Component ref={this.componentRef} {...convertedProps}>
+        {children}
+      </Component>
+    );
+  }
 }
 
-export default WebComponentWrapper
+export default WebComponentWrapper;
