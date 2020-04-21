3b353feaa3be3111e2d237ba90d0276bc86ccfca
Arturo <arturo@arturu.com>
2020-04-21 11:47:21 +0200

    Fix/lint

--- packages/components/src/components/breadcrumb/breadcrumb.spec.ts
+++ packages/components/src/components/breadcrumb/breadcrumb.spec.ts
@@ -1,15 +1,15 @@
 import { newSpecPage } from '@stencil/core/testing';
 import { Breadcrumb } from './breadcrumb';
-import { styles } from './breadcrumb.styles';
-import jss from 'jss';
+// import { styles } from './breadcrumb.styles';
+// import jss from 'jss';
 
 describe('Breadcrumb', () => {
-  let element;
+  /* let element;
   let stylesheet;
   beforeEach(async () => {
     element = new Breadcrumb();
     stylesheet = element.stylesheet = jss.createStyleSheet(styles as any);
-  });
+  }); */
 
   it('should match snapshot', async () => {
     const page = await newSpecPage({
