e1a9d8d6fdb1b53682bd89e5a7f5fe6180ebdd71
Datentyp <volkerchartier@users.noreply.github.com>
2020-04-30 15:37:58 +0200

    add a server to serve components page to puppeteer (#201)

--- packages/generator-sketch/src/inject.js
+++ packages/generator-sketch/src/inject.js
@@ -1,60 +1,67 @@
-const nodeTreeToSketchPage = require('@scaleds/html-to-sketch').nodeTreeToSketchPage;
-const puppeteer = require('puppeteer');
-const fs = require('fs');
-const path = require('path');
-const URL = require('url').URL;
-const DEBUG = false
-const crypto = require('crypto');
+const nodeTreeToSketchPage = require("@scaleds/html-to-sketch")
+  .nodeTreeToSketchPage;
+const puppeteer = require("puppeteer");
+const fs = require("fs");
+const path = require("path");
+const URL = require("url").URL;
+const DEBUG = false;
+const crypto = require("crypto");
 
-const url = 'http://localhost:3334/';
+const server = require("./renderserver");
+
+const url = "http://localhost:3334/";
 let urlObj = null;
-let directory = 'sketch-json'
+let directory = "sketch-json";
 
 if (!fs.existsSync(directory)) {
-    console.log(`generating directory: ${directory}`);
-    fs.mkdirSync(`${process.cwd()}/${directory}`, { recursive: true });
+  console.log(`generating directory: ${directory}`);
+  fs.mkdirSync(`${process.cwd()}/${directory}`, { recursive: true });
 }
 
 const outputFile = `./../${directory}/asketch.json`;
 
-puppeteer.launch({headless: !!!DEBUG ? true : false}).then(async browser => {
-    const page = await browser.newPage();
+puppeteer.launch({ headless: !!!DEBUG ? true : false }).then(async browser => {
+  const page = await browser.newPage();
 
-    if (!!!DEBUG) {
-        page.on('console', msg => console.log('PAGE LOG:', msg.text()));
+  if (!!!DEBUG) {
+    page.on("console", msg => console.log("PAGE LOG:", msg.text()));
+  }
+
+  page.on("response", async response => {
+    const url = response.url();
+    if (response.request().resourceType() === "image") {
+      response.buffer().then(file => {
+        const fileName = crypto
+          .createHash("sha1")
+          .update(url)
+          .digest("hex");
+        const filePath = path.resolve(
+          __dirname,
+          `./../${directory}/${fileName}`
+        );
+        fs.writeFileSync(filePath, file);
+      });
     }
+  });
 
-    page.on('response', async response => {
-        const url = response.url();
-        if (response.request().resourceType() === 'image') {
-            response.buffer().then(file => {
-                const fileName = crypto
-                    .createHash('sha1')
-                    .update(url)
-                    .digest('hex');
-                const filePath = path.resolve(__dirname, `./../${directory}/${fileName}`);
-                fs.writeFileSync(filePath, file);
-            });
-        }
-    });
+  await page.setViewport({ width: 1280, height: 800 });
+  await page.goto(url, {
+    waitUntil: "networkidle2"
+  });
 
-    await page.setViewport({ width: 1280, height: 800 });
-    await page.goto(url, {
-        waitUntil: 'networkidle2'
-    });
+  await page.addScriptTag({
+    path: "./dist/build/page2layers.bundle.js"
+  });
 
-    await page.addScriptTag({
-        path: './dist/build/page2layers.bundle.js'
-    });
+  const asketchPage = await page.evaluate("page2layers.run()");
 
-    const asketchPage = await page.evaluate(('page2layers.run()'));
+  fs.writeFileSync(
+    path.resolve(__dirname, outputFile),
+    JSON.stringify(asketchPage)
+  );
 
-    fs.writeFileSync(
-        path.resolve(__dirname, outputFile),
-        JSON.stringify(asketchPage)
-    );
-
-    if (!!!DEBUG) {
-        await browser.close();
-    }
+  if (!!!DEBUG) {
+    await browser.close();
+    server.close();
+  }
 });
