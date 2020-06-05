b3cee188bd39b37fdb6dda208bb82ee22fad8a5b
Daniel Beck <nowseemee@gmail.com>
2020-06-05 16:05:46 +0200

    fix: decouple from @scaleds/components, transpile @scaleds/docs feat: dynamic whitelist aka featuredComponents config property

--- packages/docs/cli/copy.js
+++ packages/docs/cli/copy.js
@@ -2,6 +2,7 @@ const findUserConfig = require("./find-user-config")
 const { logger } = require("./logger")
 const path = require("path")
 const fs = require("fs-extra")
+const spawn = require("child_process").spawn
 
 const copy = async argv => {
   try {
@@ -9,23 +10,51 @@ const copy = async argv => {
     const monumentRootPath = path.join(__dirname, "..")
     const stencilRootPath = process.env.CWD
 
-    return Promise.all([
-      fs.copy(
-        `${stencilRootPath}${config.docsFilePath}`,
-        `${monumentRootPath}/components.json`
-      ),
+    const psClean = spawn(
+      "cd",
+      [
+        path.join(__dirname, "../"),
+        "&&",
+        "yarn",
+        "gatsby",
+        "clean",
+      ],
+      {
+        shell: true,
+      }
+    )
 
-      fs.copy(
-        `${stencilRootPath}/dist/${config.namespace}`,
-        `${monumentRootPath}/public/components/`
-      ),
 
-      config.themeFilePath &&
+    psClean.on("close", () => {
+      return Promise.all([
+        fs.copy(
+          `${stencilRootPath}${config.docsFilePath}`,
+          `${monumentRootPath}/components.json`
+        ),
+
+        fs.copy(
+          `${stencilRootPath}/dist/${config.namespace}`,
+          `${monumentRootPath}/public/components/`
+        ),
+
+        fs.copy(
+          `${stencilRootPath}/dist`,
+          `${monumentRootPath}/dist`
+        ),
+
+        fs.copy(
+          `${stencilRootPath}/loader`,
+          `${monumentRootPath}/loader`
+        ),
+
         fs.copy(
           `${stencilRootPath}${config.themeFilePath}`,
           `${monumentRootPath}/public/theme.js`
         ),
-    ])
+      ])
+    })
+
+
   } catch (error) {
     logger.error(error)
   }
