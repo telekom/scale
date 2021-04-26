e582235d4360cdd794301596694e78786fe8443a
Daniel Beck <daniel@beck.win>
2021-04-26 15:24:56 +0200

    test(datagrid): improve reliability

--- packages/visual-tests/src/datagrid.visual.spec.js
+++ packages/visual-tests/src/datagrid.visual.spec.js
@@ -6,20 +6,19 @@ describe('DataGrid', () => {
     ['link-cell'],
     ['number-cell'],
     ['select-cell'],
-    ['tags-cell'],
     ['text-cell'],
     ['heading'],
     ['hide-extras'],
     ['pagination'],
-    ['selection-export'],
     ['column-stretch'],
+    ['tags-cell'],
+    ['selection-export'],
   ])('%p', async (variant) => {
     await global.page.goto(
       `http://host.docker.internal:3123/iframe.html?id=beta-components-data-grid--${variant}&viewMode=story`
     );
     await page.waitForSelector('html.hydrated');
     const previewHtml = await page.$('body');
-    await page.waitFor(3000);
     expect(await previewHtml.screenshot()).toMatchImageSnapshot();
   });
 });
