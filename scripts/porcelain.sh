1d597f0b764a09b05ae4b2334542f54ea457bace
Daniel Beck <daniel@beck.win>
2021-04-26 18:36:25 +0200

    build(pr): dedicate step for uncommitted files check

--- scripts/porcelain.sh
+++ scripts/porcelain.sh
@@ -1,13 +1,11 @@
-source ./scripts/_colors.sh;
-source ./scripts/_header.sh;
 echo "--------"
-echo "${GREEN}Check for uncommitted build outputs...${NC}"
+echo "Check for uncommitted build outputs..."
 echo "--------"
 if [ -z $(git status --porcelain) ];
 then
-    echo "${GREEN}Working copy is clean"
+    echo "Working copy is clean"
 else
-    echo "${RED}Please commit your changes"
+    echo "Please commit your changes"
     git status
     exit 1
 fi
