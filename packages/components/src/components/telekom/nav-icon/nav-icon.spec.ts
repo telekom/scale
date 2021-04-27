cdf3ddad2127428d2ac88706d5997e9580f0658c
Denis Kutlovci <Denis.kutlovci@telekom.de>
2021-04-27 15:11:47 +0200

    test: nav-icon

---
+++ packages/components/src/components/telekom/nav-icon/nav-icon.spec.ts
@@ -0,0 +1,78 @@
+import { newSpecPage, SpecPage } from '@stencil/core/testing';
+import { NavIcon } from './nav-icon';
+describe('nav-icon', () => {
+  let page: SpecPage;
+  beforeEach(async () => {
+    page = await newSpecPage({
+      components: [NavIcon],
+      html: `<scale-nav-icon></scale-nav-icon>`,
+    });
+  });
+  it('should match snapshot', async () => {
+    expect(page.root).toMatchSnapshot();
+  });
+  it('should reflect to set attributes/props', async () => {
+    const specPage = await newSpecPage({
+      components: [NavIcon],
+      html: `<scale-nav-icon
+                is-active="true"
+                href="href"
+                click-link="true"
+                icon="icon"
+                is-mobile-menu-open="true"
+                ref-mobile-menu-toggle="">
+              </scale-nav-icon>`,
+    });
+    expect(specPage.rootInstance.isActive).toBe(true);
+    expect(specPage.rootInstance.href).toBe('href');
+    expect(specPage.rootInstance.clickLink).toBe('true');
+    expect(specPage.rootInstance.icon).toBe('icon');
+    expect(specPage.rootInstance.isMobileMenuOpen).toBe(true);
+    expect(specPage.rootInstance.refMobileMenuToggle).toBe('');
+  });
+  it('should trigger onClick()', async () => {
+    page.root.clickLink = jest.fn();
+    const anchor = page.root.querySelector(
+      '.meta-navigation__item-link'
+    ) as HTMLElement;
+    await page.waitForChanges();
+    anchor.click();
+    await page.waitForChanges();
+    expect(page.root.clickLink).toHaveBeenCalled();
+  });
+  it('should trigger onKeyDown', async () => {
+    page.root.clickLink = jest.fn();
+    page.root.refMobileMenuToggle = () => {};
+    const anchor = page.root.querySelector(
+      '.meta-navigation__item-link'
+    ) as HTMLElement;
+    await page.waitForChanges();
+    anchor.dispatchEvent(
+      new KeyboardEvent('keydown', {
+        view: window,
+        bubbles: true,
+        cancelable: true,
+        key: 'Escape',
+      })
+    );
+    await page.waitForChanges();
+    expect(page.root.clickLink).toHaveBeenCalled();
+  });
+  it('should trigger onClick()', async () => {
+    page.root.clickLink = jest.fn();
+    const anchor = page.root.querySelector(
+      '.meta-navigation__item-link'
+    ) as HTMLElement;
+    await page.waitForChanges();
+    anchor.dispatchEvent(
+      new KeyboardEvent('keydown', {
+        view: window,
+        bubbles: true,
+        cancelable: true,
+        key: 'Escape',
+      })
+    );
+    await page.waitForChanges();
+    expect(page.root.clickLink).not.toHaveBeenCalled();
+  });
+});
