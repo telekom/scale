febe562ba81f7020a7939dc6847e5f74992c325e
marvinLaubenstein <marvin.laubenstein@telekom.de>
2021-04-29 11:29:00 +0200

    fix: [formating][test file]

--- packages/components/src/components/tab-panel/tab-panel.spec.ts
+++ packages/components/src/components/tab-panel/tab-panel.spec.ts
@@ -1,27 +1,27 @@
-import { newSpecPage } from "@stencil/core/testing";
-import { TabPanel } from "./tab-panel";
+import { newSpecPage } from '@stencil/core/testing';
+import { TabPanel } from './tab-panel';
 
 describe('TabPanel', () => {
-    let page;
-    beforeEach(async () => {
-      page = await newSpecPage({
-        components: [TabPanel],
-        html: `<scale-tab-panel></scale-tab-panel>`,
-      });
+  let page;
+  beforeEach(async () => {
+    page = await newSpecPage({
+      components: [TabPanel],
+      html: `<scale-tab-panel></scale-tab-panel>`,
     });
-    it('should match snapshot', async () => {
-      expect(page.root).toMatchSnapshot();
-    });
-    it('should handle styles', async()=>{
-        page.rootInstance.styles = 'background-color:red'
-        await page.waitForChanges();
-        expect(page.root).toMatchSnapshot();
-    })
-    it('should handle css classes', async()=>{
-        const tabPanel = new TabPanel()
-        tabPanel.small = true
-        expect(tabPanel.getCssClassMap()).toBe('tab-panel tab-panel--small')
-        tabPanel.small = false
-        expect(tabPanel.getCssClassMap()).toBe('tab-panel')
-    })
+  });
+  it('should match snapshot', async () => {
+    expect(page.root).toMatchSnapshot();
+  });
+  it('should handle styles', async () => {
+    page.rootInstance.styles = 'background-color:red';
+    await page.waitForChanges();
+    expect(page.root).toMatchSnapshot();
+  });
+  it('should handle css classes', async () => {
+    const tabPanel = new TabPanel();
+    tabPanel.small = true;
+    expect(tabPanel.getCssClassMap()).toBe('tab-panel tab-panel--small');
+    tabPanel.small = false;
+    expect(tabPanel.getCssClassMap()).toBe('tab-panel');
+  });
 });
