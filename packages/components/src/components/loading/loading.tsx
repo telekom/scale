f4772bb68309ed2050d1588d7ae64c198ce61062
Daniel Beck <daniel@beck.win>
2021-05-03 16:21:34 +0200

    refactor(spinner): css variables, theming, parts, better props

--- packages/components/src/components/loading/loading.tsx
+++ packages/components/src/components/loading/loading.tsx
@@ -7,42 +7,47 @@ import classNames from 'classnames';
   shadow: true,
 })
 export class Loading {
-  @Prop() variant: 'white' | 'magenta' = 'magenta';
+  @Prop() variant: 'white' | 'primary' = 'primary';
   @Prop() alignment: 'horizontal' | 'vertical' = 'horizontal';
-  @Prop() text: boolean = false;
-
-  getLoadingText() {
-    return this.alignment === 'horizontal' ? (
-      <div class="spinner-text--alignment-horizontal">Loading...</div>
-    ) : (
-      <div class="spinner-text--alignment-vertical">Loading ...</div>
-    );
-  }
-
-  styles() {
-    return `:host {
-        --loading-color: ${this.variant === 'white' ? '#fff' : '#e20074'};
-      }`;
-  }
+  @Prop() text: string = 'Loading...';
 
   render() {
     return (
       <Host>
-        <style>{this.styles()}</style>
-        <div class={this.getCssClassMap()}>
-          <div class="spinner-container">
-            <div class="spinner-circle"></div>
+        <div part={this.getBasePartMap()} class={this.getCssClassMap()}>
+          <div part="container" class="spinner__container">
+            <div part="circle" class="spinner__circle"></div>
           </div>
-          {this.text ? this.getLoadingText() : false}
+          <div
+            part="text"
+            class="spinner__text"
+            aria-live="polite"
+            aria-busy="true"
+          >
+            {this.text}
+          </div>
         </div>
       </Host>
     );
   }
 
+  getBasePartMap() {
+    return this.getCssOrBasePartMap('basePart');
+  }
+
   getCssClassMap() {
-    return (
-      classNames(`spinner`),
-      this.alignment && `spinner--alignment-${this.alignment}`
+    return this.getCssOrBasePartMap('css');
+  }
+
+  getCssOrBasePartMap(mode: 'basePart' | 'css') {
+    const name = 'spinner';
+    const prefix = mode === 'basePart' ? '' : `${name}--`;
+
+    return classNames(
+      name,
+      this.alignment && `${prefix}alignment-${this.alignment}`,
+      this.variant && `${prefix}variant-${this.variant}`,
+      !this.text && `${prefix}text-hidden`
     );
   }
 }
