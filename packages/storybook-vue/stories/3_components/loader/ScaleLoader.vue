e1f2ff62399a618fe37b821254f7934547f12ca1
marvinLaubenstein <marvin.laubenstein@telekom.de>
2021-05-05 15:10:47 +0200

    feat: adding first stories

---
+++ packages/storybook-vue/stories/3_components/loader/ScaleLoader.vue
@@ -0,0 +1,25 @@
+<template>
+  <scale-loader
+    :variant="variant"
+    :alignment="alignment"
+    :text="text"
+    :size="size"
+  >
+  </scale-loader>
+</template>
+
+<script>
+import { action } from '@storybook/addon-actions';
+
+export default {
+  props: {
+    variant: { type: 'white' | 'magenta', default: 'magenta' },
+    alignment: {
+      type: 'horizontal' | 'vertical',
+      default: 'horizontal',
+    },
+    text: { type: String },
+    size: { type: 'small' | 'large', default: 'small' },
+  },
+};
+</script>
