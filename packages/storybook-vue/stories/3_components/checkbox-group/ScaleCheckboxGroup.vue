8cfa2fa09d3102bb38fa0dd54a791b3bfdef02b3
marvinLaubenstein <marvin.laubenstein@telekom.de>
2021-05-12 10:25:48 +0200

    feat: adding stories

---
+++ packages/storybook-vue/stories/3_components/checkbox-group/ScaleCheckboxGroup.vue
@@ -0,0 +1,15 @@
+<template>
+  <scale-checkbox-group>
+    <slot></slot>
+    <slot></slot>
+  </scale-checkbox-group>
+</template>
+
+<script>
+import { action } from '@storybook/addon-actions';
+
+export default {
+  props: {},
+  methods: {},
+};
+</script>
