3715e7f2cd2538962889bdfb303b4ffda37afa20
Denis Kutlovci <Denis.kutlovci@telekom.de>
2021-05-12 12:05:41 +0200

    icon input

--- packages/components/src/components/alertbox/alertbox.tsx
+++ packages/components/src/components/alertbox/alertbox.tsx
@@ -1,5 +1,15 @@
-import { Component, h, Host, Prop, Element, State, Event, EventEmitter } from '@stencil/core';
+import {
+  Component,
+  h,
+  Host,
+  Prop,
+  Element,
+  State,
+  Event,
+  EventEmitter,
+} from '@stencil/core';
 import classNames from 'classnames';
+
 @Component({
   tag: 'scale-alertbox',
   styleUrl: 'alertbox.css',
@@ -7,35 +17,50 @@ import classNames from 'classnames';
 })
 export class Alertbox {
   @Prop() size?: 'small' | 'big' = 'small';
-  @Prop() color?: 'white' | 'black' | 'magenta' = 'magenta';
+  @Prop() color?: 'white' | 'black' | 'error'  = 'black';
   @Prop() variant?: 'floatingShadow' | 'outline';
-  @Prop() iconVariant?: 'check' | 'error';
+  @Prop() iconVariant: boolean;
   @Prop({ reflect: true }) hasCloseButton? = false;
   @State() content: boolean = true;
   @Element() element: HTMLElement;
-  @Event() scaleCloseAlert: EventEmitter<MouseEvent>; 
+  @Event() scaleCloseAlert: EventEmitter<MouseEvent>;
+
   componentDidLoad() {
     this.content = !!this.element.querySelector("p[slot='text']");
+    if(this.color === "error"){
+      this.iconVariant = true;
+    }
   }
-  handleClose(event) {
 
+  handleClose(event) {
     this.scaleCloseAlert.emit();
-    console.log(event)
+    console.log(event);
   }
+
+
   render() {
-    console.log("THIS HAS CLOSE BUTTON: ",this.hasCloseButton);
     return (
       <Host>
         <div class={this.getCssClassMap()}>
           <div class="alertbox_container">
             <div class="alertbox_container_header">
+
+              {this.iconVariant ? (
+                <scale-icon-alert-error accessibility-title="error" />
+              ) : ( 
+                <scale-icon-action-success accessibility-title="success" />
+              )}
+
               <header class="alert_heading">
                 <slot name="header">Missing Title</slot>
-                {this.hasCloseButton && <scale-icon-action-circle-close 
-                onClick={(event)=>{
-                  this.handleClose(event);
-                }}
-                accessibility-title="circle-close" />}
+                {this.hasCloseButton && (
+                  <scale-icon-action-circle-close
+                    onClick={(event) => {
+                      this.handleClose(event);
+                    }}
+                    accessibility-title="circle-close"
+                  />
+                )}
               </header>
             </div>
             {this.content && (
@@ -48,6 +73,7 @@ export class Alertbox {
       </Host>
     );
   }
+
   getCssClassMap() {
     return classNames(
       'Alertbox',
