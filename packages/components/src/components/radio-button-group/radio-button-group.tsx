6e978c42305a0716fa36f0a0e1bfc445a525135f
marvinLaubenstein <marvin.laubenstein@telekom.de>
2021-05-20 14:22:59 +0200

    feat: final radio button group

--- packages/components/src/components/radio-button-group/radio-button-group.tsx
+++ packages/components/src/components/radio-button-group/radio-button-group.tsx
@@ -1,17 +1,25 @@
-import { Component, h, Host } from '@stencil/core';
-
+import { Component, h, Host, Prop } from '@stencil/core';
 @Component({
   tag: 'scale-radio-button-group',
   styleUrl: './radio-button-group.css',
   shadow: true,
 })
 export class CheckboxGroup {
+  /** (optional) Input label */
+  @Prop() label: string = '';
+  /** (optional) Input helper text */
+  @Prop() helperText?: string = '';
   render() {
     return (
       <Host>
         <div class="radio-button-group">
           <div class="radio-button-group__label">
-            <slot name="radio-button-label" />
+            <h1 class="radio-button-group__title">{this.label}</h1>
+            {this.helperText && (
+              <div class="radio-button-group__helper-text">
+                {this.helperText}
+              </div>
+            )}
           </div>
           <div class="radio-button-group__container">
             <slot name="radio-button-list" />
