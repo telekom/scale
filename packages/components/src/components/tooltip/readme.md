9c53a12f77838c3e16fd6a008a5e4fc62aa4b162
marvinLaubenstein <marvin.laubenstein@telekom.de>
2021-06-08 17:51:22 +0200

    feat: adding tooltip with popperJS included

--- packages/components/src/components/tooltip/readme.md
+++ packages/components/src/components/tooltip/readme.md
@@ -5,6 +5,80 @@
 <!-- Auto Generated Below -->
 
 
+## Properties
+
+| Property    | Attribute   | Description                                                                                                                                                                                                                                   | Type                                                                                                                                                                 | Default         |
+| ----------- | ----------- | --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- | -------------------------------------------------------------------------------------------------------------------------------------------------------------------- | --------------- |
+| `content`   | `content`   | The tooltip's content. Alternatively, you can use the content slot.                                                                                                                                                                           | `string`                                                                                                                                                             | `''`            |
+| `disabled`  | `disabled`  | Set to true to disable the tooltip so it won't show when triggered.                                                                                                                                                                           | `boolean`                                                                                                                                                            | `false`         |
+| `distance`  | `distance`  | The distance in pixels from which to offset the tooltip away from its target.                                                                                                                                                                 | `number`                                                                                                                                                             | `10`            |
+| `open`      | `open`      | Indicates whether or not the tooltip is open. You can use this in lieu of the show/hide methods.                                                                                                                                              | `boolean`                                                                                                                                                            | `false`         |
+| `placement` | `placement` | The preferred placement of the tooltip. Note that the actual placement may vary as needed to keep the tooltip inside of the viewport.                                                                                                         | `"bottom" \| "bottom-end" \| "bottom-start" \| "left" \| "left-end" \| "left-start" \| "right" \| "right-end" \| "right-start" \| "top" \| "top-end" \| "top-start"` | `'top'`         |
+| `skidding`  | `skidding`  | The distance in pixels from which to offset the tooltip along its target.                                                                                                                                                                     | `number`                                                                                                                                                             | `0`             |
+| `trigger`   | `trigger`   | Controls how the tooltip is activated. Possible options include `click`, `hover`, `focus`, and `manual`. Multiple options can be passed by separating them with a space. When manual is used, the tooltip must be activated programmatically. | `string`                                                                                                                                                             | `'hover focus'` |
+
+
+## Events
+
+| Event           | Description                                                                                                  | Type               |
+| --------------- | ------------------------------------------------------------------------------------------------------------ | ------------------ |
+| `sl-after-hide` | Emitted after the tooltip has hidden and all transitions are complete.                                       | `CustomEvent<any>` |
+| `sl-aftershow`  | Emitted after the tooltip has shown and all transitions are complete.                                        | `CustomEvent<any>` |
+| `sl-hide`       | Emitted when the tooltip begins to hide. Calling `event.preventDefault()` will prevent it from being hidden. | `CustomEvent<any>` |
+| `sl-show`       | Emitted when the tooltip begins to show. Calling `event.preventDefault()` will prevent it from being shown.  | `CustomEvent<any>` |
+
+
+## Methods
+
+### `hide() => Promise<void>`
+
+Shows the tooltip.
+
+#### Returns
+
+Type: `Promise<void>`
+
+
+
+### `show() => Promise<void>`
+
+Shows the tooltip.
+
+#### Returns
+
+Type: `Promise<void>`
+
+
+
+
+## Slots
+
+| Slot        | Description                                                                      |
+| ----------- | -------------------------------------------------------------------------------- |
+|             | The tooltip's target element. Only the first element will be used as the target. |
+| `"content"` | The tooltip's content. Alternatively, you can use the content prop.              |
+
+
+## Shadow Parts
+
+| Part     | Description                   |
+| -------- | ----------------------------- |
+| `"base"` | The component's base wrapper. |
+
+
+## CSS Custom Properties
+
+| Name                     | Description                                                  |
+| ------------------------ | ------------------------------------------------------------ |
+| `--hide-delay`           | The amount of time to wait before hiding the tooltip.        |
+| `--hide-duration`        | The amount of time the hide transition takes to complete.    |
+| `--hide-timing-function` | The timing function (easing) to use for the hide transition. |
+| `--max-width`            | The maximum width of the tooltip.                            |
+| `--show-delay`           | The amount of time to wait before showing the tooltip.       |
+| `--show-duration`        | The amount of time the show transition takes to complete.    |
+| `--show-timing-function` | The timing function (easing) to use for the show transition. |
+
+
 ----------------------------------------------
 
 *Built with [StencilJS](https://stenciljs.com/)*
--- packages/components/src/components/tooltip/tooltip.tsx
+++ packages/components/src/components/tooltip/tooltip.tsx
@@ -1,55 +1,295 @@
-import { Component, h, Host, Prop } from '@stencil/core';
+import {
+  Component,
+  Element,
+  Event,
+  EventEmitter,
+  Host,
+  Method,
+  Prop,
+  Watch,
+  h,
+} from '@stencil/core';
 import classNames from 'classnames';
+import Popover from './utilities/popover';
+
+let id = 0;
+
+/**
+ * @since 2.0
+ * @status stable
+ *
+ * @slot - The tooltip's target element. Only the first element will be used as the target.
+ * @slot content - The tooltip's content. Alternatively, you can use the content prop.
+ *
+ * @part base - The component's base wrapper.
+ */
 
 @Component({
   tag: 'scale-tooltip',
-  styleUrl: './tooltip.css',
+  styleUrl: 'tooltip.css',
   shadow: true,
 })
 export class Tooltip {
-  /** (optional) Alignment of the tooltip*/
-  @Prop() alignment: 'left' | 'right' | 'top' | 'bottom' = 'top';
-  /** (optional) Text that is displayed in the tooltip*/
-  @Prop() label: string = 'Tooltip is missing';
-  /** (optional) Width of the tooltip (Text aligns to tooltip width)*/
-  @Prop() width: number = 66;
+  componentId = `tooltip-${++id}`;
+  isVisible = false;
+  popover: Popover;
+  tooltipPositioner: HTMLElement;
+  target: HTMLElement;
+  tooltip: any;
 
-  getStyle() {
-    return `:host {
-      --tooltip-width: ${this.width}px;
-    }`;
+  @Element() host: HTMLScaleTooltipElement;
+
+  /** The tooltip's content. Alternatively, you can use the content slot. */
+  @Prop() content = '';
+
+  /**
+   * The preferred placement of the tooltip. Note that the actual placement may vary as needed to keep the tooltip
+   * inside of the viewport.
+   */
+  @Prop() placement:
+    | 'top'
+    | 'top-start'
+    | 'top-end'
+    | 'right'
+    | 'right-start'
+    | 'right-end'
+    | 'bottom'
+    | 'bottom-start'
+    | 'bottom-end'
+    | 'left'
+    | 'left-start'
+    | 'left-end' = 'top';
+
+  /** Set to true to disable the tooltip so it won't show when triggered. */
+  @Prop() disabled = false;
+
+  /** The distance in pixels from which to offset the tooltip away from its target. */
+  @Prop() distance = 10;
+
+  /** Indicates whether or not the tooltip is open. You can use this in lieu of the show/hide methods. */
+  @Prop({ mutable: true, reflect: true }) open = false;
+
+  /** The distance in pixels from which to offset the tooltip along its target. */
+  @Prop() skidding = 0;
+
+  /**
+   * Controls how the tooltip is activated. Possible options include `click`, `hover`, `focus`, and `manual`. Multiple
+   * options can be passed by separating them with a space. When manual is used, the tooltip must be activated
+   * programmatically.
+   */
+  @Prop() trigger: string = 'hover focus';
+
+  @Watch('open')
+  handleOpenChange() {
+    this.open ? this.show() : this.hide();
+  }
+
+  /** Emitted when the tooltip begins to show. Calling `event.preventDefault()` will prevent it from being shown. */
+  @Event({ eventName: 'scale-show' }) scaleShow: EventEmitter;
+
+  /** Emitted after the tooltip has shown and all transitions are complete. */
+  @Event({ eventName: 'scale-aftershow' }) scaleAfterShow: EventEmitter;
+
+  /** Emitted when the tooltip begins to hide. Calling `event.preventDefault()` will prevent it from being hidden. */
+  @Event({ eventName: 'scale-hide' }) scaleHide: EventEmitter;
+
+  /** Emitted after the tooltip has hidden and all transitions are complete. */
+  @Event({ eventName: 'scale-after-hide' }) scaleAfterHide: EventEmitter;
+
+  connectedCallback() {
+    this.handleBlur = this.handleBlur.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.handleFocus = this.handleFocus.bind(this);
+    this.handleKeyDown = this.handleKeyDown.bind(this);
+    this.handleMouseOver = this.handleMouseOver.bind(this);
+    this.handleMouseOut = this.handleMouseOut.bind(this);
+    this.handleSlotChange = this.handleSlotChange.bind(this);
+  }
+
+  componentDidLoad() {
+    this.target = this.getTarget();
+    this.popover = new Popover(this.target, this.tooltipPositioner);
+    this.syncOptions();
+
+    this.host.addEventListener('blur', this.handleBlur, true);
+    this.host.addEventListener('click', this.handleClick, true);
+    this.host.addEventListener('focus', this.handleFocus, true);
+
+    // Show on init if open
+    this.tooltipPositioner.hidden = !this.open;
+    if (this.open) {
+      this.show();
+    }
+  }
+
+  componentDidUpdate() {
+    this.syncOptions();
+  }
+
+  disconnectedCallback() {
+    this.popover.destroy();
+
+    this.host.removeEventListener('blur', this.handleBlur, true);
+    this.host.removeEventListener('click', this.handleClick, true);
+    this.host.removeEventListener('focus', this.handleFocus, true);
+  }
+
+  /** Shows the tooltip. */
+  @Method()
+  async show() {
+    // Prevent subsequent calls to the method, whether manually or triggered by the `open` watcher
+    if (this.isVisible) {
+      return;
+    }
+
+    const scaleShow = this.scaleShow.emit();
+    if (scaleShow.defaultPrevented) {
+      this.open = false;
+      return;
+    }
+
+    this.isVisible = true;
+    this.open = true;
+    this.popover.show();
+  }
+
+  /** Shows the tooltip. */
+  @Method()
+  async hide() {
+    // Prevent subsequent calls to the method, whether manually or triggered by the `open` watcher
+    if (!this.isVisible) {
+      return;
+    }
+
+    const scaleHide = this.scaleHide.emit();
+    if (scaleHide.defaultPrevented) {
+      this.open = true;
+      return;
+    }
+
+    this.isVisible = false;
+    this.open = false;
+    this.popover.hide();
+  }
+
+  getTarget() {
+    // Get the first child that isn't a <style> or content slot
+    const target = [...this.host.children].find(
+      (el) =>
+        el.tagName.toLowerCase() !== 'style' &&
+        el.getAttribute('slot') !== 'content'
+    ) as HTMLElement;
+
+    if (!target) {
+      throw new Error('Invalid tooltip target: no child element was found.');
+    }
+
+    return target;
+  }
+
+  handleBlur() {
+    if (this.hasTrigger('focus')) {
+      this.hide();
+    }
+  }
+
+  handleClick() {
+    if (this.hasTrigger('click')) {
+      this.open ? this.hide() : this.show();
+    }
+  }
+
+  handleFocus() {
+    if (this.hasTrigger('focus')) {
+      this.show();
+    }
+  }
+
+  handleKeyDown(event: KeyboardEvent) {
+    // Pressing escape when the target element has focus should dismiss the tooltip
+    if (this.open && event.key === 'Escape') {
+      event.stopPropagation();
+      this.hide();
+    }
+  }
+
+  handleMouseOver() {
+    if (this.hasTrigger('hover')) {
+      this.show();
+    }
+  }
+
+  handleMouseOut() {
+    if (this.hasTrigger('hover')) {
+      this.hide();
+    }
+  }
+
+  handleSlotChange() {
+    const oldTarget = this.target;
+    const newTarget = this.getTarget();
+
+    if (newTarget !== oldTarget) {
+      if (oldTarget) {
+        oldTarget.removeAttribute('aria-describedby');
+      }
+      newTarget.setAttribute('aria-describedby', this.componentId);
+    }
+  }
+
+  hasTrigger(triggerType: string) {
+    const triggers = this.trigger.split(' ');
+    return triggers.includes(triggerType);
+  }
+
+  syncOptions() {
+    this.popover.setOptions({
+      placement: this.placement,
+      distance: this.distance,
+      skidding: this.skidding,
+      transitionElement: this.tooltip,
+      onAfterHide: () => this.scaleAfterHide.emit(),
+      onAfterShow: () => this.scaleAfterShow.emit(),
+    });
   }
 
   render() {
     return (
-      <Host>
-        <style>{this.getStyle()}</style>
-        <a
-          part={this.getBasePartMap()}
-          class={this.getCssClassMap()}
-          data-tooltip={this.label}
-        >
-          <slot></slot>
-        </a>
+      <Host
+        onKeyDown={this.handleKeyDown}
+        onMouseOver={this.handleMouseOver}
+        onMouseOut={this.handleMouseOut}
+      >
+        <slot onSlotchange={this.handleSlotChange} />
+
+        {!this.disabled && (
+          <div
+            ref={(el) => (this.tooltipPositioner = el)}
+            class={this.getCssClassMap()}
+          >
+            <div
+              part="base"
+              ref={(el) => (this.tooltip = el)}
+              id={this.componentId}
+              class={{
+                tooltip: true,
+                'tooltip--open': this.open,
+              }}
+              role="tooltip"
+              aria-hidden={this.open ? 'false' : 'true'}
+            >
+              <slot name="content">{this.content}</slot>
+            </div>
+          </div>
+        )}
       </Host>
     );
   }
 
-  getBasePartMap() {
-    return this.getCssOrBasePartMap('basePart');
-  }
-
   getCssClassMap() {
-    return this.getCssOrBasePartMap('css');
-  }
-
-  getCssOrBasePartMap(mode: 'basePart' | 'css') {
-    const name = 'tooltip';
-    const prefix = mode === 'basePart' ? '' : `${name}--`;
-
     return classNames(
-      name,
-      this.alignment && `${prefix}alignment-${this.alignment}`
+      `tooltip-positioner`,
+      this.placement && `tooltip-positioner--placement-${this.placement}`
     );
   }
 }
