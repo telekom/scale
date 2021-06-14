0f3bb315ff7ff11c75da4f6858caa9d32e06cb55
marvinLaubenstein <marvin.laubenstein@telekom.de>
2021-06-14 14:13:33 +0200

    refactor: close and open logic (similar to alert)

--- packages/components/src/components/alertbox/alertbox.tsx
+++ packages/components/src/components/alertbox/alertbox.tsx
@@ -1,5 +1,6 @@
 import { Component, h, Host, Prop, Element, State } from '@stencil/core';
 import classNames from 'classnames';
+import statusNote from '../../utils/status-note';
 
 @Component({
   tag: 'scale-alertbox',
@@ -10,12 +11,20 @@ export class Alertbox {
   @Prop() color?: 'white' | 'black' | 'error' = 'white';
   @Prop() variant?: 'floating' | 'outline';
   @Prop() icon: boolean = false;
-  @Prop({ reflect: true }) close? = false;
+  @Prop({ reflect: true }) hasClose? = false;
+  @Prop({ reflect: true }) opened: boolean;
+  @Prop() timeout?: boolean | number = false;
   @State() content: boolean = true;
-  @Element() element: HTMLElement;
+  @Element() hostElement: HTMLElement;
+
+  defaultTimeout = 2000;
 
   componentDidLoad() {
-    this.content = !!this.element.querySelector("p[slot='text']");
+    this.content = !!this.hostElement.querySelector("p[slot='text']");
+  }
+
+  connectedCallback() {
+    statusNote({ source: this.hostElement, type: 'warn' });
   }
 
   handleIcons() {
@@ -46,21 +55,45 @@ export class Alertbox {
     }
     return;
   }
-  
+
+  close() {
+    this.opened = false;
+    console.log('close');
+  }
+
+  onCloseAlertWithTimeout = () => {
+    if (this.timeout !== false) {
+      if (typeof this.timeout === 'number') {
+        setTimeout(this.close, this.timeout);
+      } else {
+        setTimeout(this.close, this.defaultTimeout);
+      }
+    } else {
+      return null;
+    }
+  };
 
   render() {
+    this.onCloseAlertWithTimeout();
+
+    if (!this.opened) {
+      return null;
+    }
+
     return (
       <Host>
         <div part={this.getBasePartMap()} class={this.getCssClassMap()}>
           <div part="container" class="alertbox__container">
             {this.handleIcons()}
             <header part="header" class="alertbox__container-header">
-              <slot name="header"><p>Missing Title</p></slot>
-              {this.close && (
+              <slot name="header">
+                <p>Missing Title</p>
+              </slot>
+              {this.hasClose && (
                 <scale-icon-action-circle-close
                   class="alertbox__icon-close"
                   onClick={() => {
-                    document.querySelector('scale-alertbox').remove();
+                    this.close();
                   }}
                   accessibility-title="close"
                 />
