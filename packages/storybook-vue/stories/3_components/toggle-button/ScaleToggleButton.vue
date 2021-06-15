bd3df7d818c4c4979ac24ceacd2e905c6748f646
Christian Pajung <christian.pajung@telekom.de>
2021-06-15 14:36:16 +0200

    feat: add toggle button to storybook-vue

---
+++ packages/storybook-vue/stories/3_components/toggle-button/ScaleToggleButton.vue
@@ -0,0 +1,31 @@
+<template>
+  <scale-toggle-button
+    :size="size"
+    :variant="variant"
+    :color-scheme="colorScheme"
+    :disabled="disabled"
+    :selected="selected"
+    :icon-only="iconOnly"
+    :radius="radius"
+    :aria-label="ariaLabel"
+    :styles="styles"
+  >
+    <slot></slot>
+  </scale-toggle-button>
+</template>
+
+<script>
+export default {
+  props: {
+    size: { type: String, default: 'large' },
+    variant: { type: String, default: 'primary' },
+    colorScheme: { type: String, default: 'magenta' },
+    disabled: { type: Boolean, default: false },
+    selected: { type: Boolean, default: false },
+    iconOnly: { type: Boolean, default: false },
+    radius: { type: String },
+    ariaLabel: { type: String },
+    styles: { type: String },
+  },
+};
+</script>
