1ce3f6237940a547d2a5d2d10de595257005552e
Christian Pajung <christian.pajung@telekom.de>
2021-06-16 17:27:31 +0200

    feat: add toggle group stories

---
+++ packages/storybook-vue/stories/3_components/toggle-group/ScaleToggleGroup.vue
@@ -0,0 +1,29 @@
+<template>
+  <scale-toggle-group
+    :size="size"
+    :variant="variant"
+	:box-type="boxType"
+    :disabled="disabled"
+    :border="border"
+    :multi="multi"
+    :aria-label="ariaLabel"
+    :styles="styles"
+  >
+    <slot></slot>
+  </scale-toggle-group>
+</template>
+
+<script>
+export default {
+  props: {
+    size: { type: String, default: 'large' },
+    variant: { type: String, default: 'primary' },
+    boxType: { type: String, default: 'inline' },
+    disabled: { type: Boolean, default: false },
+    border: { type: Boolean, default: false },
+    multi: { type: Boolean, default: true },
+    ariaLabel: { type: String },
+    styles: { type: String },
+  },
+};
+</script>
