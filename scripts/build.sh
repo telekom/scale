9b0f47023c4de9a2affb63f686fb0e3c49c31d82
Arturo Castillo Delgado <ac@iconstorm.com>
2021-07-02 09:05:49 +0200

    chore: implement password protected staging server, tidy up scripts (#434)

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,11 +1,17 @@
 source ./scripts/_colors.sh;
 source ./scripts/_header.sh;
 echo "--------"
-echo "${GREEN}Build Components...${NC}"
+echo "${GREEN}Bootstrapping...${NC}"
+yarn bootstrap;
 echo "--------"
-echo "${GREEN}Build Components...${NC}"
+echo "${GREEN}Building components...${NC}"
 yarn workspace @telekom/scale-components build;
 echo "--------"
-echo "${GREEN}Build React wrappers...${NC}"
+echo "${GREEN}Building React package...${NC}"
 yarn workspace @telekom/scale-components-react build;
 echo "--------"
+echo "${GREEN}Building Vue package...${NC}"
+yarn workspace @telekom/scale-components-vue build;
+echo "--------"
+echo "${GREEN}Building Angular package...${NC}"
+yarn workspace @telekom/scale-components-angular build;
