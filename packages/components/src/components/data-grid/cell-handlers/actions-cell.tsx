9fbf265211abd469ad03391f1be8aeeba95632ac
Jano Sagi <39010293+js31415@users.noreply.github.com>
2021-07-09 15:31:44 +0200

    feat(data-grid) Add actions cell type

---
+++ packages/components/src/components/data-grid/cell-handlers/actions-cell.tsx
@@ -0,0 +1,37 @@
+/**
+ * @license
+ * Scale https://github.com/telekom/scale
+ *
+ * Copyright (c) 2021 Egor Kirpichev and contributors, Deutsche Telekom AG
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+ import { h } from '@stencil/core';
+ import { Cell } from './cell-interface';
+ 
+ export const ActionsCell: Cell = {
+   defaults: {},
+   getLongestContent({ rows, columnIndex }) {
+     // Skip check as content width is always the same
+     return rows[0][columnIndex];
+   },
+   render: ({content}) => {
+     
+     return (
+        <div class={`tbody__actions`}>
+            {content.map((action) => {
+                const {label, ...props} = action
+                return (
+                    <scale-button {...props}>
+                            {label}
+                    </scale-button>
+                )   
+            })}
+        </div>
+     );
+   },
+ };
+ 
