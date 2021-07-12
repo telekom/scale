6830570b5c7c7e5838d1b10d6a914e03543acfb2
marvinLaubenstein <marvin.laubenstein@telekom.de>
2021-07-12 17:33:04 +0200

    fix: standard story

--- packages/components/src/components/alertbox/alertbox.tsx
+++ packages/components/src/components/alertbox/alertbox.tsx
@@ -1,13 +1,14 @@
 import { Component, h, Host, Prop, Element, State } from '@stencil/core';
 import classNames from 'classnames';
 import statusNote from '../../utils/status-note';
+
 @Component({
   tag: 'scale-alertbox',
   styleUrl: 'alertbox.css',
   shadow: true,
 })
 export class Alertbox {
-  @Prop() color?: 'white' | 'yellow' | 'green' | 'blue' | 'black' | 'error' = 'white';
+  @Prop() color?: 'white' | 'black' | 'error' = 'white';
   @Prop() variant?: 'floating' | 'outline';
   @Prop() icon: boolean = false;
   @Prop({ reflect: true }) hasclose?: boolean = false;
@@ -15,13 +16,17 @@ export class Alertbox {
   @Prop() timeout?: boolean | number = false;
   @State() content: boolean = true;
   @Element() hostElement: HTMLElement;
-  defaultTimeout = 20000;
+
+  defaultTimeout = 2000;
+
   componentDidLoad() {
     this.content = !!this.hostElement.querySelector("p[slot='text']");
   }
+
   connectedCallback() {
     statusNote({ source: this.hostElement, type: 'warn' });
   }
+
   handleIcons() {
     if (this.icon) {
       switch (this.color) {
@@ -39,27 +44,6 @@ export class Alertbox {
               accessibility-title="success"
             />
           );
-          case 'yellow':
-            return (
-              <scale-icon-action-warning
-                class="alertbox__icon-warning"
-                accessibility-title="warning"
-              />
-            );
-            case 'blue':
-            return (
-              <scale-icon-action-informational
-                class="alertbox__icon-info"
-                accessibility-title="info"
-              />
-            );
-            case 'green':
-            return (
-              <scale-icon-action-success
-                class="alertbox__icon-success"
-                accessibility-title="warsuccessning"
-              />
-            );
         case 'error':
           return (
             <scale-icon-alert-error
@@ -71,10 +55,12 @@ export class Alertbox {
     }
     return;
   }
-  close = () => {
+
+  close() {
     this.opened = false;
     console.log('close');
   }
+
   onCloseAlertWithTimeout = () => {
     if (this.timeout !== false) {
       if (typeof this.timeout === 'number') {
@@ -86,11 +72,14 @@ export class Alertbox {
       return null;
     }
   };
+
   render() {
     this.onCloseAlertWithTimeout();
+
     if (!this.opened) {
       return null;
     }
+
     return (
       <Host>
         <div part={this.getBasePartMap()} class={this.getCssClassMap()}>
@@ -120,15 +109,19 @@ export class Alertbox {
       </Host>
     );
   }
+
   getBasePartMap() {
     return this.getCssOrBasePartMap('basePart');
   }
+
   getCssClassMap() {
     return this.getCssOrBasePartMap('css');
   }
+
   getCssOrBasePartMap(mode: 'basePart' | 'css') {
     const name = 'alertbox';
     const prefix = mode === 'basePart' ? '' : `${name}--`;
+
     return classNames(
       name,
       this.color && `${prefix}color-${this.color}`,
