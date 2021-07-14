25b001eaf84cc6947fa92c7640ad576364bbf5cd
Christian Pajung <christian.pajung@telekom.de>
2021-07-14 17:06:11 +0200

    feat: avoid parent's borders to overlap component

--- packages/components/src/components/pagination/pagination.tsx
+++ packages/components/src/components/pagination/pagination.tsx
@@ -88,7 +88,6 @@ export class Pagination {
   calculateWidth() {
     // calculate max possible width
     this.maxWidth = (this.totalElements.toString().length * 4 + 3) * 9;
-    console.log(this.maxWidth)
   }
 
   /* 8. Public Methods */
@@ -147,7 +146,11 @@ export class Pagination {
               viewBox="0 0 48 52"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
-              stroke={isAtStart ? 'var(--scl-color-grey-10)' : 'var(--scl-color-grey-70'}
+              stroke={
+                isAtStart
+                  ? 'var(--scl-color-grey-10)'
+                  : 'var(--scl-color-grey-70'
+              }
             >
               <path
                 d="M44.5 48.5L21.5 26L44.5 3.5M27.5 48.5L4.5 26L27.5 3.5"
@@ -168,7 +171,11 @@ export class Pagination {
               viewBox="0 0 37 52"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
-              stroke={isAtStart ? 'var(--scl-color-grey-10)' : 'var(--scl-color-grey-70'}
+              stroke={
+                isAtStart
+                  ? 'var(--scl-color-grey-10)'
+                  : 'var(--scl-color-grey-70'
+              }
             >
               <path
                 d="M33 48L6 26L33 4"
@@ -199,7 +206,9 @@ export class Pagination {
               viewBox="0 0 37 52"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
-              stroke={isAtEnd ? 'var(--scl-color-grey-10)' : 'var(--scl-color-grey-70'}
+              stroke={
+                isAtEnd ? 'var(--scl-color-grey-10)' : 'var(--scl-color-grey-70'
+              }
             >
               <path
                 d="M4 4L31 26L4 48"
@@ -220,7 +229,9 @@ export class Pagination {
               viewBox="0 0 48 52"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
-              stroke={isAtEnd ? 'var(--scl-color-grey-10)' : 'var(--scl-color-grey-70'}
+              stroke={
+                isAtEnd ? 'var(--scl-color-grey-10)' : 'var(--scl-color-grey-70'
+              }
             >
               <path
                 d="M3.5 3.5L26.5 26L3.5 48.5M20.5 3.5L43.5 26L20.5 48.5"
