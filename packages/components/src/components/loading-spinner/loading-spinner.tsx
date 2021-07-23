0480ab191131e624672449078c9d671e9f82c874
marvinLaubenstein <marvin.laubenstein@telekom.de>
2021-07-23 13:46:16 +0200

    feat: loading spinner background

--- packages/components/src/components/loading-spinner/loading-spinner.tsx
+++ packages/components/src/components/loading-spinner/loading-spinner.tsx
@@ -25,7 +25,16 @@ export class LoadingSpinner {
                   cy="25"
                   r="20"
                   fill="none"
-                  stroke={this.variant === 'white' ? 'white' : '#E20074'}
+                  stroke-width="5"
+                ></circle>
+              </svg>
+              <svg class="spinner-background" viewBox="0 0 50 50">
+                <circle
+                  class="path"
+                  cx="25"
+                  cy="25"
+                  r="20"
+                  fill="none"
                   stroke-width="5"
                 ></circle>
               </svg>
