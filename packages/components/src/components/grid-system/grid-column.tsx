325ebc85829af72dc89b9c66ab72a647a2f00575
Chris P <pajungc@gmx.de>
2021-08-17 14:10:51 +0200

    feat: adaptions

--- packages/components/src/components/grid-system/grid-column.tsx
+++ packages/components/src/components/grid-system/grid-column.tsx
@@ -9,12 +9,14 @@ export class GridColumn {
   @Element() hostElement: HTMLElement;
   /** (optional) Injected CSS styles */
   @Prop() styles?: string;
+  /** (optional) width: x of 12 */
+  @Prop() width?: number;
 
   render() {
     return (
       <Host>
         <style>{this.styles}</style>
-        <div class="grid-column">
+        <div class={`grid-column ${this.width && 'width-6'}`}>
 			<slot />
 		</div>
       </Host>
