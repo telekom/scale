76f42ac81cabcf4880f6ccf80706f6b649f8d5f7
marvinLaubenstein <marvin.laubenstein@telekom.de>
2021-09-01 16:52:07 +0200

    refactor: linting

--- packages/visual-tests/src/tooltip.visual.spec.js
+++ packages/visual-tests/src/tooltip.visual.spec.js
@@ -1,13 +1,10 @@
 describe('Tooltip', () => {
-  test.each([['opendefault']])(
-    '%p',
-    async (variant) => {
-      await global.page.goto(
-        `http://host.docker.internal:3123/iframe.html?id=beta-components-tooltip--${variant}&viewMode=story`
-      );
-      await page.waitForSelector('html.hydrated');
-      const previewHtml = await page.$('body');
-      expect(await previewHtml.screenshot()).toMatchImageSnapshot();
-    }
-  );
+  test.each([['opendefault']])('%p', async (variant) => {
+    await global.page.goto(
+      `http://host.docker.internal:3123/iframe.html?id=beta-components-tooltip--${variant}&viewMode=story`
+    );
+    await page.waitForSelector('html.hydrated');
+    const previewHtml = await page.$('body');
+    expect(await previewHtml.screenshot()).toMatchImageSnapshot();
+  });
 });
