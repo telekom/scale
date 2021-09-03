0bc635415524202e855681a07673ac35eff9a652
Chris P <pajungc@gmx.de>
2021-09-03 15:54:55 +0200

    feat: enable screens of hover,- focus,- and active states

--- packages/visual-tests/src/button.visual.spec.js
+++ packages/visual-tests/src/button.visual.spec.js
@@ -22,6 +22,18 @@ describe('Button', () => {
     );
     await page.waitForSelector('html.hydrated');
     const previewHtml = await page.$('body');
+    const button = await page.evaluateHandle(
+      `document.querySelector("#root scale-button").shadowRoot.querySelector(".button")`
+    );
+   /*  await button.focus();
+    expect(await previewHtml.screenshot()).toMatchImageSnapshot()
+    await button.hover();
+    await page.waitFor(200);
+    await button.hover();
+    expect(await previewHtml.screenshot()).toMatchImageSnapshot(); */
+    await page.mouse.move(20, 20);
+    await page.mouse.down();
+    await page.waitFor(200);
     expect(await previewHtml.screenshot()).toMatchImageSnapshot();
   });
 });
