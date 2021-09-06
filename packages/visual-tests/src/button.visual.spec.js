879b730a879784dc3c7c92c8b0fb1e20046bc9da
Chris P <pajungc@gmx.de>
2021-09-06 12:15:29 +0200

    feat: button with focus, active, hover

--- packages/visual-tests/src/button.visual.spec.js
+++ packages/visual-tests/src/button.visual.spec.js
@@ -25,14 +25,17 @@ describe('Button', () => {
     const button = await page.evaluateHandle(
       `document.querySelector("#root scale-button").shadowRoot.querySelector(".button")`
     );
-   /*  await button.focus();
-    expect(await previewHtml.screenshot()).toMatchImageSnapshot()
+    expect(await previewHtml.screenshot()).toMatchImageSnapshot();
     await button.hover();
     await page.waitFor(200);
-    await button.hover();
-    expect(await previewHtml.screenshot()).toMatchImageSnapshot(); */
+    expect(await previewHtml.screenshot()).toMatchImageSnapshot();
     await page.mouse.move(20, 20);
     await page.mouse.down();
+    await page.waitFor(200);
+    expect(await previewHtml.screenshot()).toMatchImageSnapshot();
+    await page.mouse.up();
+    await page.mouse.move(0, 0);
+    await button.focus();
     await page.waitFor(200);
     expect(await previewHtml.screenshot()).toMatchImageSnapshot();
   });
