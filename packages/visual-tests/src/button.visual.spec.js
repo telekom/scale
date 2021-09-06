893aa5b46bbf92e592dcb4efa3abc4c5a7e66081
Chris P <pajungc@gmx.de>
2021-09-06 14:58:57 +0200

    feat: set transitions to 0s

--- packages/visual-tests/src/button.visual.spec.js
+++ packages/visual-tests/src/button.visual.spec.js
@@ -20,23 +20,35 @@ describe('Button', () => {
     await global.page.goto(
       `http://host.docker.internal:3123/iframe.html?id=components-button--${variant}&viewMode=story`
     );
+
     await page.waitForSelector('html.hydrated');
+
+    await page.evaluate(() => {
+      const transitions = [
+        '--scl-motion-duration-immediate',
+        '--scl-motion-duration-fast',
+        '--scl-motion-duration-slower',
+        '--scl-motion-duration-deliberate',
+      ];
+      
+      transitions.forEach((transitionSpeed) => {
+        document.body.style.setProperty(transitionSpeed, '0s');
+      });
+    }); 
+    
     const previewHtml = await page.$('body');
     const button = await page.evaluateHandle(
       `document.querySelector("#root scale-button").shadowRoot.querySelector(".button")`
     );
     expect(await previewHtml.screenshot()).toMatchImageSnapshot();
     await button.hover();
-    await page.waitFor(200);
     expect(await previewHtml.screenshot()).toMatchImageSnapshot();
     await page.mouse.move(20, 20);
     await page.mouse.down();
-    await page.waitFor(200);
     expect(await previewHtml.screenshot()).toMatchImageSnapshot();
     await page.mouse.up();
     await page.mouse.move(0, 0);
     await button.focus();
-    await page.waitFor(200);
     expect(await previewHtml.screenshot()).toMatchImageSnapshot();
   });
 });
