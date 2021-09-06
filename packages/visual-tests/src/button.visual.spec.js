a4249a625f777eb1e677ed4df36b3b976caa4b35
Chris P <pajungc@gmx.de>
2021-09-06 17:20:05 +0200

    test: outsource functionality

--- packages/visual-tests/src/button.visual.spec.js
+++ packages/visual-tests/src/button.visual.spec.js
@@ -1,4 +1,5 @@
 describe('Button', () => {
+  const {setTrasitionToZero} = require('../utils')
   test.each([
     ['standard'],
     ['secondary'],
@@ -24,16 +25,7 @@ describe('Button', () => {
     await page.waitForSelector('html.hydrated');
 
     await page.evaluate(() => {
-      const transitions = [
-        '--scl-motion-duration-immediate',
-        '--scl-motion-duration-fast',
-        '--scl-motion-duration-slower',
-        '--scl-motion-duration-deliberate',
-      ];
-      
-      transitions.forEach((transitionSpeed) => {
-        document.body.style.setProperty(transitionSpeed, '0s');
-      });
+      setTrasitionToZero();
     }); 
     
     const previewHtml = await page.$('body');
