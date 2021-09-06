750ab46f950e449bafc63923d9c199e88e76d25d
marvinLaubenstein <marvin.laubenstein@telekom.de>
2021-09-06 19:37:26 +0200

    refactor: accessibility

--- packages/components/src/components/radio-button-group/radio-button-group.tsx
+++ packages/components/src/components/radio-button-group/radio-button-group.tsx
@@ -14,21 +14,29 @@ export class RadioButtonGroup {
   @Prop() status?: string = '';
 
   render() {
-    const ariaLabel = this.label + ' ' + this.helperText;
-
     return (
       <form>
         <fieldset class="radio-button-group">
-          <div
-            class="radio-button-group__label"
-            aria-label={this.helperText ? ariaLabel : this.label}
-            tabindex="0"
-          >
-            <legend class="radio-button-group__title">{this.label}</legend>
+          <legend class="radio-button-group__title">
+            <label
+              role="text"
+              class="radio-button-group__title-label"
+              aria-label={this.label}
+              tabindex="0"
+            >
+              {this.label}
+            </label>
             {this.helperText ? (
-              <div class={this.getCssClassMap()}>{this.helperText}</div>
+              <div
+                role="text"
+                class={this.getCssClassMap()}
+                aria-label={this.helperText}
+                tabindex="0"
+              >
+                {this.helperText}
+              </div>
             ) : null}
-          </div>
+          </legend>
           <div class="radio-button-group__container">
             <slot />
           </div>
