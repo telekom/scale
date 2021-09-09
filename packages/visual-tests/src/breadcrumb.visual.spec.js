b39de4c37dc8a5241fd50c8b5e50e5561eafacc0
Chris P <pajungc@gmx.de>
2021-09-09 14:13:32 +0200

    feat: visual tests for breadcrumb

--- packages/visual-tests/src/breadcrumb.visual.spec.js
+++ packages/visual-tests/src/breadcrumb.visual.spec.js
@@ -5,12 +5,28 @@ describe('Breadcrumb', () => {
     );
     await page.waitForSelector('html.hydrated');
 
+    await page.evaluate(() => {
+      const transitions = [
+        '--scl-motion-duration-immediate',
+        '--scl-motion-duration-fast',
+        '--scl-motion-duration-slower',
+        '--scl-motion-duration-deliberate',
+      ];
+
+      transitions.forEach((transitionSpeed) => {
+        document.body.style.setProperty(transitionSpeed, '0s');
+      });
+    });
+    const previewHtml = await page.$('body');
     const firstLink = await page.evaluateHandle(
       `document.querySelector("#root > scale-breadcrumb").shadowRoot.querySelector("nav > ol > li:nth-child(1) > a")`
     );
+    expect(await previewHtml.screenshot()).toMatchImageSnapshot();
     await firstLink.hover();
-
-    const previewHtml = await page.$('body');
+    expect(await previewHtml.screenshot()).toMatchImageSnapshot();
+    // mouse down on firstlink
+    await page.mouse.move(40, 30);
+    await page.mouse.down();
     expect(await previewHtml.screenshot()).toMatchImageSnapshot();
   });
 });
