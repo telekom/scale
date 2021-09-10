bc4dc25aeffc6be2d2fa62a96a64b911536c988b
Chris P <pajungc@gmx.de>
2021-09-10 11:33:31 +0200

    feat: visual tests for brand-header

--- packages/visual-tests/src/brand-header.visual.spec.js
+++ packages/visual-tests/src/brand-header.visual.spec.js
@@ -1,31 +1,50 @@
 describe('Brand Header', () => {
-  it('hovering in mega menu', async () => {
+  it('default mega menu states', async () => {
     await global.page.goto(
       `http://host.docker.internal:3123/iframe.html?id=components-brand-header-navigation--standard&viewMode=story`
     );
     await page.waitForSelector('html.hydrated');
 
+    await page.evaluate(() => {
+      const transitions = [
+        '--scl-motion-duration-immediate',
+        '--scl-motion-duration-fast',
+        '--scl-motion-duration-slower',
+        '--scl-motion-duration-deliberate',
+      ];
+
+      transitions.forEach((transitionSpeed) => {
+        document.body.style.setProperty(transitionSpeed, '0s');
+      });
+    });
+
+    const previewHtml = await page.$('body');
     const firstLink = await page.evaluateHandle(
       `document.querySelector("#root > div > scale-app-shell").shadowRoot.querySelector("scale-app-header nav.header__nav > div > div.header__nav-menu-wrapper > div.header__nav-menu-main > ul > scale-nav-main:nth-child(1) > li > a")`
     );
     await firstLink.hover();
-
-    const previewHtml = await page.$('body');
+    expect(await previewHtml.screenshot()).toMatchImageSnapshot();
+    await firstLink.focus();
+    expect(await previewHtml.screenshot()).toMatchImageSnapshot();
+    await page.evaluate(() => {
+      window.scrollBy(0, window.innerHeight);
+    });
     expect(await previewHtml.screenshot()).toMatchImageSnapshot();
   });
 
-  it('hovering in custom mega menu', async () => {
+  it('custom mega menu states', async () => {
     await global.page.goto(
       `http://host.docker.internal:3123/iframe.html?id=components-brand-header-navigation--custom-main-navigation&viewMode=story`
     );
     await page.waitForSelector('html.hydrated');
 
+    const previewHtml = await page.$('body');
     const firstLink = await page.evaluateHandle(
       `document.querySelector("#nav-main-with-mega-menu > li > a")`
     );
     await firstLink.hover();
-
-    const previewHtml = await page.$('body');
+    expect(await previewHtml.screenshot()).toMatchImageSnapshot();
+    await firstLink.focus();
     expect(await previewHtml.screenshot()).toMatchImageSnapshot();
   });
 
@@ -41,6 +60,19 @@ describe('Brand Header', () => {
       `http://host.docker.internal:3123/iframe.html?id=components-brand-header-navigation--${variant}&viewMode=story`
     );
     await page.waitForSelector('html.hydrated');
+
+    await page.evaluate(() => {
+      const transitions = [
+        '--scl-motion-duration-immediate',
+        '--scl-motion-duration-fast',
+        '--scl-motion-duration-slower',
+        '--scl-motion-duration-deliberate',
+      ];
+
+      transitions.forEach((transitionSpeed) => {
+        document.body.style.setProperty(transitionSpeed, '0s');
+      });
+    });
 
     const previewHtml = await page.$('body');
     expect(await previewHtml.screenshot()).toMatchImageSnapshot();
