b13881d2c6d484c392ecfc2186f7f4de4e0d5cf8
Chris P <pajungc@gmx.de>
2021-09-16 16:23:09 +0200

    feat: visual tests for checkbox

--- packages/visual-tests/src/checkbox.visual.spec.js
+++ packages/visual-tests/src/checkbox.visual.spec.js
@@ -15,4 +15,40 @@ describe('Checkbox', () => {
     const previewHtml = await page.$('body');
     expect(await previewHtml.screenshot()).toMatchImageSnapshot();
   });
+  test.each([['standard'], ['selected'], ['custom-label']])(
+    '%p',
+    async (variant) => {
+      await global.page.goto(
+        `http://host.docker.internal:3123/iframe.html?id=components-checkbox--${variant}&viewMode=story`
+      );
+      await page.waitForSelector('html.hydrated');
+
+      await page.evaluate(() => {
+        const transitions = [
+          '--scl-motion-duration-immediate',
+          '--scl-motion-duration-fast',
+          '--scl-motion-duration-slower',
+          '--scl-motion-duration-deliberate',
+        ];
+
+        transitions.forEach((transitionSpeed) => {
+          document.body.style.setProperty(transitionSpeed, '0s');
+        });
+      });
+
+      const previewHtml = await page.$('body');
+      const checkbox = await page.evaluateHandle(
+        `document.querySelector("#root > scale-checkbox > label")`
+      );
+      await checkbox.hover();
+      expect(await previewHtml.screenshot()).toMatchImageSnapshot();
+      await page.mouse.move(20, 20);
+      await page.mouse.down();
+      expect(await previewHtml.screenshot()).toMatchImageSnapshot();
+      await page.mouse.up();
+      await page.mouse.move(0, 0);
+      await checkbox.focus();
+      expect(await previewHtml.screenshot()).toMatchImageSnapshot();
+    }
+  );
 });
