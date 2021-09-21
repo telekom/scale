519c8121acdb3a7f11c4cb214dc7b19d92a83819
Stefan Kopco <hey@oddcelot.dev>
2021-09-21 19:05:06 +0200

    refactor(link): overhaul implementation

--- packages/components/src/components/link/link.tsx
+++ packages/components/src/components/link/link.tsx
@@ -10,7 +10,6 @@
  */
 
 import { Component, h, Prop, Host } from '@stencil/core';
-import classNames from 'classnames';
 
 @Component({
   tag: 'scale-link',
@@ -22,45 +21,62 @@ export class Link {
   @Prop() href: string;
   /** (optional) Disabled link */
   @Prop() disabled?: boolean = false;
+  /** (optional) Download declaration */
+  @Prop() download?: boolean = false;
   /** (optional) Block link */
   @Prop() block?: boolean = false;
-  /** (optional) Link underline */
-  @Prop() omitUnderline?: boolean = false;
   /** (optional) Link open a new tag */
-  @Prop() target?: string = '_self';
-  /** (optional) Link variant */
-  @Prop() variant?: string = '';
+  @Prop() target?: string;
+  /** (optional) Remove the `text-decoration` from the text (can also be achieved via `--text-decoration: none`)  */
+  @Prop() omitUnderline?: boolean = false;
+  /** (optional) Chnage icon/content slot order */
+  @Prop() iconPosition: 'before' | 'after' = 'after';
+  /** (optional) attatch additional anchor tag attributes (`hreflang`, `ping`, `referrerpolicy`, `rel`, `type`) */
+  @Prop() anchorAttributes;
   /** (optional) Injected CSS styles */
   @Prop() styles?: string;
 
-  render() {
-    return (
-      <Host>
-        {this.styles && <style>{this.styles}</style>}
-        <a
-          tabindex={0}
-          class={this.getCssClassMap()}
-          part={classNames('base', this.disabled && 'disabled')}
-          href={this.disabled ? 'javascript:void(0)' : this.href}
-          {...(!this.disabled ? { target: this.target } : {})}
-          aria-disabled={String(this.disabled)}
-        >
-          <span class="link__wrapper" part="wrapper">
-            <slot />
-            <slot name="icon" />
-          </span>
-        </a>
-      </Host>
-    );
+  renderSlots() {
+    const slots = [<slot />, <slot name="icon" />];
+
+    // if (this.iconPosition === 'before') slots.reverse();
+
+    return slots;
   }
 
-  getCssClassMap() {
-    return classNames(
-      'link',
-      this.disabled && 'link--disabled',
-      this.block && 'link--block',
-      !this.omitUnderline && 'link--underline',
-      this.omitUnderline && 'link--no-underline'
+  getAnchorProps() {
+    const props = {
+      download: this.download || null,
+      tabIndex: this.disabled ? -1 : null,
+      target: this.target || null,
+      href: this.href || null,
+      'aria-disabled': this.disabled,
+    };
+    return { ...props, ...this.anchorAttributes };
+  }
+
+  render() {
+    return (
+      <Host
+        class={{
+          disabled: this.disabled,
+          block: this.block,
+          // underlined: !this.omitUnderline,
+          reverse: this.iconPosition === 'before',
+        }}
+      >
+        {this.omitUnderline && (
+          <style>{`
+            :host{
+              --text-decoration: none;
+            }
+        `}</style>
+        )}
+        {this.styles && <style>{this.styles}</style>}
+        <a part="anchor" {...this.getAnchorProps()}>
+          <span part="wrapper">{this.renderSlots()}</span>
+        </a>
+      </Host>
     );
   }
 }
