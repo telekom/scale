f4b341836569990cf432c7ec7d3dd1245018b451
marvinLaubenstein <marvin.laubenstein@telekom.de>
2021-09-28 11:39:36 +0200

    test: adding test cases

--- packages/components/src/components/menu-flyout/menu-flyout.spec.ts
+++ packages/components/src/components/menu-flyout/menu-flyout.spec.ts
@@ -4,57 +4,36 @@ import { MenuFlyout } from './menu-flyout';
 jest.useFakeTimers();
 
 describe('MenuFlyout‚', () => {
+  let page;
+  beforeEach(async () => {
+    page = await newSpecPage({
+      components: [MenuFlyout],
+      html: `
+                    <scale-menu-flyout>
+                        <div slot="trigger" style="padding: 20px; cursor: pointer; background: var(--scl-color-blue-40)"></div>
+                        <scale-menu-flyout-list>
+                            <scale-menu-flyout-item>Item 1</scale-menu-flyout-item>
+                            <scale-menu-flyout-item>Item 2</scale-menu-flyout-item>
+                            <scale-menu-flyout-item>Item 3</scale-menu-flyout-item>
+                        </scale-menu-flyout-list>
+                    </scale-menu-flyout>`,
+    });
+  });
   describe('snapshot tests', () => {
     it('should handle menu flyout items', async () => {
-      const page = await newSpecPage({
-        components: [MenuFlyout],
-        html: `
-                <scale-menu-flyout>
-                    <div slot="trigger" style="padding: 20px; cursor: pointer; background: var(--scl-color-blue-40)"></div>
-                    <scale-menu-flyout-list>
-                        <scale-menu-flyout-item>Item 1</scale-menu-flyout-item>
-                        <scale-menu-flyout-item>Item 2</scale-menu-flyout-item>
-                        <scale-menu-flyout-item>Item 3</scale-menu-flyout-item>
-                    </scale-menu-flyout-list>
-                </scale-menu-flyout>`,
-      });
       expect(page.root).toMatchSnapshot();
     });
   });
   describe('prop test', async () => {
     it('should handle props', async () => {
-      const page = await newSpecPage({
-        components: [MenuFlyout],
-        html: `
-                <scale-menu-flyout>
-                    <div slot="trigger" style="padding: 20px; cursor: pointer; background: var(--scl-color-blue-40)"></div>
-                    <scale-menu-flyout-list>
-                        <scale-menu-flyout-item>Item 1</scale-menu-flyout-item>
-                        <scale-menu-flyout-item>Item 2</scale-menu-flyout-item>
-                        <scale-menu-flyout-item>Item 3</scale-menu-flyout-item>
-                    </scale-menu-flyout-list>
-                </scale-menu-flyout>`,
-      });
       const setDirection = 'bottom-right';
       page.rootInstance.direction = setDirection;
 
       expect(page.rootInstance.direction).toBe(setDirection);
     });
   });
-  describe('on slot change test', async () => {
+  describe('slot change', async () => {
     it('should handle slot change', async () => {
-      const page = await newSpecPage({
-        components: [MenuFlyout],
-        html: `
-                  <scale-menu-flyout>
-                      <div slot="trigger" style="padding: 20px; cursor: pointer; background: var(--scl-color-blue-40)"></div>
-                      <scale-menu-flyout-list>
-                          <scale-menu-flyout-item>Item 1</scale-menu-flyout-item>
-                          <scale-menu-flyout-item>Item 2</scale-menu-flyout-item>
-                          <scale-menu-flyout-item>Item 3</scale-menu-flyout-item>
-                      </scale-menu-flyout-list>
-                  </scale-menu-flyout>`,
-      });
       page.root.shadowRoot.querySelector('slot').removeAttribute('slot');
       expect(page.root).toMatchSnapshot();
     });
@@ -64,11 +43,37 @@ describe('MenuFlyout‚', () => {
       element.open = true;
       expect(element.getCssClassMap()).toContain('menu--open');
     });
-    it('should handle on Catcher', async () => {
+  });
+  describe('on catcher events', async () => {
+    it('should handle on Catcher click', async () => {
       let element = new MenuFlyout();
       const spy = jest.spyOn(element, 'emitOpenState');
       element.open = true;
       element.onCatcherClick(new Event('click'));
+      expect(spy).toBeCalled();
+      expect(element.open).toBeFalsy;
+    });
+    it('should handle on Catcher scroll', async () => {
+      let element = new MenuFlyout();
+      const spy = jest.spyOn(element, 'emitOpenState');
+      element.open = true;
+      element.onCatcherScroll();
+      expect(spy).toBeCalled();
+      expect(element.open).toBeFalsy;
+    });
+    it('should handle key down', async () => {
+      let element = new MenuFlyout();
+      const spy = jest.spyOn(element, 'toggleOpenState');
+      element.open = false;
+      element.handleKeyDown(new KeyboardEvent('ArrowDown'));
+      expect(spy).toBeCalled();
+      expect(element.open).toBeFalsy;
+    });
+    it('should handle on trigger click', async () => {
+      let element = new MenuFlyout();
+      const spy = jest.spyOn(element, 'toggleOpenState');
+      element.open = false;
+      element.onTriggerClick();
       expect(spy).toBeCalled();
       expect(element.open).toBeFalsy;
     });
