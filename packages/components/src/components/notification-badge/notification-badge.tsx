e5d6b6fbe34401ffeef5a8072485f29e3773cc61
marvinLaubenstein <marvin.laubenstein@telekom.de>
2021-10-20 17:40:40 +0200

    feat: handle character number of label

--- packages/components/src/components/notification-badge/notification-badge.tsx
+++ packages/components/src/components/notification-badge/notification-badge.tsx
@@ -1,5 +1,6 @@
 import { Component, h, Host, Prop } from '@stencil/core';
 import classNames from 'classnames';
+import { isNaN } from 'lodash';
 
 @Component({
   tag: 'scale-notification-badge',
@@ -8,15 +9,36 @@ import classNames from 'classnames';
 })
 export class NotificationBadge {
   /** (optional) Text that is displayed in the badge*/
-  @Prop() label: string;
+  @Prop() label: string | number;
+  @Prop() maxCharacters: number = 3;
   @Prop() type: 'icon' | 'text' = 'icon';
+
+  getLabel() {
+    if (this.label) {
+      if (!isNaN(this.label)) {
+        let labelNumber = String(this.label);
+        if (labelNumber.length > this.maxCharacters) {
+          labelNumber = labelNumber.substring(0, this.maxCharacters);
+          labelNumber += '+';
+        }
+        return labelNumber;
+      } else {
+        let labelString = String(this.label);
+        if (labelString.length > this.maxCharacters) {
+          labelString = labelString.substring(0, this.maxCharacters);
+          labelString += '...';
+        }
+        return labelString;
+      }
+    }
+  }
 
   render() {
     return (
       <Host>
         <a class={this.getCssClassMap()}>
           <slot />
-          <span class="notification-badge__circle">{this.label}</span>
+          <span class="notification-badge__circle">{this.getLabel()}</span>
         </a>
       </Host>
     );
