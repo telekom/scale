e6a5a961acc8e99337f86d170f821548c5361395
Daniel Beck <daniel@beck.win>
2021-10-28 11:51:59 +0200

    test(visual): bust more transitions

--- packages/visual-tests/src/datagrid.visual.spec.js
+++ packages/visual-tests/src/datagrid.visual.spec.js
@@ -17,7 +17,18 @@ describe('DataGrid', () => {
     );
     await page.waitForSelector('html.hydrated');
     const previewHtml = await page.$('body');
-    await page.waitFor(1000);
+    await page.evaluate(() => {
+      const transitions = [
+        '--scl-motion-duration-immediate',
+        '--scl-motion-duration-fast',
+        '--scl-motion-duration-slower',
+        '--scl-motion-duration-deliberate',
+      ];
+
+      transitions.forEach((transitionSpeed) => {
+        document.body.style.setProperty(transitionSpeed, '0s');
+      });
+    });
     expect(await previewHtml.screenshot()).toMatchImageSnapshot();
   });
 });
