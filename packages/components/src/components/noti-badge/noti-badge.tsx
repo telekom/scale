44637fbc6ab23a5bc0be404c9a6f71b7937312a0
marvinLaubenstein <marvin.laubenstein@telekom.de>
2021-11-01 19:20:15 +0100

    feat: adding label formating

--- packages/components/src/components/noti-badge/noti-badge.tsx
+++ packages/components/src/components/noti-badge/noti-badge.tsx
@@ -9,14 +9,39 @@ import classNames from 'classnames';
 export class NotificationBadge {
   /** (optional) Text that is displayed in the badge*/
   @Prop() label: number;
+  @Prop() maxCharacters: number = 3;
   @Prop() type: 'icon' | 'text' | 'nav-icon' = 'icon';
+
+  getLabel() {
+    if (this.label) {
+      if (!isNaN(this.label)) {
+        let labelNumber = '' + this.label;
+        if (labelNumber.length > this.maxCharacters) {
+          const SI_SYMBOL = ['', 'k', 'M', 'G', 'T', 'P', 'E'];
+          const tier = Math.floor(Math.log10(Number(this.label)) / 3) || 0;
+          if (tier > 0) {
+            const scaled = Number(this.label) / Math.pow(10, tier * 3);
+            labelNumber = scaled.toFixed(1).replace('.0', '') + SI_SYMBOL[tier];
+          }
+        }
+        return labelNumber;
+      } else {
+        let labelString = String(this.label);
+        if (labelString.length > this.maxCharacters) {
+          labelString = labelString.substring(0, this.maxCharacters);
+          labelString += '...';
+        }
+        return labelString;
+      }
+    }
+  }
 
   render() {
     return (
       <Host>
         <a class={this.getCssClassMap()}>
           <slot />
-          <span class="notfication-badge__circle">{this.label}</span>
+          <span class="notfication-badge__circle">{this.getLabel()}</span>
         </a>
       </Host>
     );
