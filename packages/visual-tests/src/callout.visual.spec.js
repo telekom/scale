1f7506453473610200cb8e8f5a6c4a45cfa513f6
Arturo <ac@iconstorm.com>
2021-11-02 12:51:23 +0100

    style: format

--- packages/visual-tests/src/callout.visual.spec.js
+++ packages/visual-tests/src/callout.visual.spec.js
@@ -1,13 +1,16 @@
 describe('Callout', () => {
-  test.each([['standard'], ['sizes'], ['rotation'], ['variants'], ['asterisk']])(
-    '%p',
-    async (variant) => {
-      await global.page.goto(
-        `http://host.docker.internal:3123/iframe.html?id=beta-components-callout--${variant}&viewMode=story`
-      );
-      await page.waitForSelector('html.hydrated');
-      const previewHtml = await page.$('body');
-      expect(await previewHtml.screenshot()).toMatchImageSnapshot();
-    }
-  );
+  test.each([
+    ['standard'],
+    ['sizes'],
+    ['rotation'],
+    ['variants'],
+    ['asterisk'],
+  ])('%p', async (variant) => {
+    await global.page.goto(
+      `http://host.docker.internal:3123/iframe.html?id=beta-components-callout--${variant}&viewMode=story`
+    );
+    await page.waitForSelector('html.hydrated');
+    const previewHtml = await page.$('body');
+    expect(await previewHtml.screenshot()).toMatchImageSnapshot();
+  });
 });
