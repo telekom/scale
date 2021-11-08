5c6507d765c78793d1a47d3bb65555252848880d
Daniel Beck <daniel@beck.win>
2021-11-08 19:50:12 +0100

    fix(border): update color

--- packages/visual-tests/src/tooltip.visual.spec.js
+++ packages/visual-tests/src/tooltip.visual.spec.js
@@ -1,4 +1,4 @@
-describe.only('Tooltip', () => {
+describe('Tooltip', () => {
   test.each([['standard']])('%p', async (variant) => {
     await global.page.goto(
       `http://host.docker.internal:3123/iframe.html?id=beta-components-tooltip--${variant}&viewMode=story`
