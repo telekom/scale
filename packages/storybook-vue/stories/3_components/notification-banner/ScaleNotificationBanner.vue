732f7f6b178fa9981631bf224c13cd3ebc496b66
marvinLaubenstein <marvin.laubenstein@telekom.de>
2021-11-15 17:35:25 +0100

    feat: stories for link and text

---
+++ packages/storybook-vue/stories/3_components/notification-banner/ScaleNotificationBanner.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <slot name="text" />
+    <slot name="link" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    variant: {
+      type: 'error' | 'warning' | 'success' | 'informational',
+      default: 'informational',
+    },
+    dismissible: { type: Boolean, default: false },
+    opened: { type: Boolean },
+    timeout: { type: Boolean | Number, default: false },
+  },
+};
+</script>
