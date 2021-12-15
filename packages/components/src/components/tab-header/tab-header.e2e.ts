e0ca5d7df479700903cd92c5ff6c3e172ecef536
Arturo Castillo Delgado <ac@iconstorm.com>
2021-12-15 13:06:46 +0100

    feat: adapt icon size automatically based on context (#725). Fixes #702

--- packages/components/src/components/tab-header/tab-header.e2e.ts
+++ packages/components/src/components/tab-header/tab-header.e2e.ts
@@ -11,31 +11,24 @@
 
 import { newE2EPage } from '@stencil/core/testing';
 
-describe('scale-button', () => {
+describe('scale-tab-header', () => {
   it('renders', async () => {
     const page = await newE2EPage();
-    await page.setContent('<scale-button>Click me!</scale-button>');
-    const element = await page.find('scale-button');
-    expect(element).toHaveClass('hydrated');
-  });
-
-  it('should set tabindex on its inner element', async () => {
-    const page = await newE2EPage();
     await page.setContent(
-      '<scale-button inner-tabindex="5">Click me!</scale-button>'
+      '<scale-tab-header slot="tab">General</scale-tab-header>'
     );
-    const element = await page.find('scale-button >>> button');
-    expect(element.getAttribute('tabindex')).toBe('5');
+    const element = await page.find('scale-tab-header');
+    expect(element).toHaveClass('hydrated');
   });
 
   it('should set size for icons', async () => {
     const page = await newE2EPage();
     await page.setContent(`
-      <scale-button size="small">
-        <scale-icon-action-search></scale-icon-action-search> Label
-      </scale-button>
+      <scale-tab-header>
+        <scale-icon-home-home></scale-icon-home-home> General
+      </scale-tab-header>
     `);
-    const element = await page.find('scale-icon-action-search');
+    const element = await page.find('scale-icon-home-home');
     expect(element.getAttribute('size')).toBe('16');
   });
 });
