4aede97dc310facac885c3722ee3defcbf69c28e
kuro <sergey.grebenyukov@telekom.com>
2021-12-18 23:20:52 +0300

    feat: grid beta

---
+++ packages/components/src/components/grid-item/grid-item.tsx
@@ -0,0 +1,32 @@
+/**
+ * @license
+ * Scale https://github.com/telekom/scale
+ *
+ * Copyright (c) 2021, Deutsche Telekom AG
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+import { Component, Prop, h, Host, Element } from '@stencil/core';
+
+@Component({
+  tag: 'scale-grid-item',
+  shadow: false,
+})
+export class Col {
+  @Element() hostElement: HTMLElement;
+
+  /** (optional) Injected CSS styles */
+  @Prop() styles?: string;
+
+  render() {
+    return (
+      <Host>
+        {this.styles && <style>{this.styles}</style>}
+        <slot />
+      </Host>
+    );
+  }
+}
---
+++ packages/components/src/components/grid/readme.md
@@ -0,0 +1,17 @@
+# scale-grid
+
+
+
+<!-- Auto Generated Below -->
+
+
+## Properties
+
+| Property | Attribute | Description                    | Type     | Default     |
+| -------- | --------- | ------------------------------ | -------- | ----------- |
+| `styles` | `styles`  | (optional) Injected CSS styles | `string` | `undefined` |
+
+
+----------------------------------------------
+
+*Built with [StencilJS](https://stenciljs.com/)*
