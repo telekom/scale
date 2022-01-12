fea3422c470d6e2fd91c675454fd1df8fed15606
christopherGdynia <A105227211@BE1GC757>
2022-01-12 12:40:13 +0100

    tab-nav props with default value as optional

--- packages/components/src/components/tab-nav/tab-nav.tsx
+++ packages/components/src/components/tab-nav/tab-nav.tsx
@@ -40,7 +40,7 @@ export class TabNav {
 
   /** True for smaller height and font size in tab headers. */
   /** @deprecated - size should replace small */
-  @Prop() small: boolean = false;
+  @Prop() small?: boolean = false;
   /** (optional) size  */
   @Prop() size: 'small' | 'large' = 'large';
   /** (optional) Injected CSS styles */
