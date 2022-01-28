3a20bbd4b4cb6f2df3687ee188ad7d78ea48968b
Arturo <ac@iconstorm.com>
2022-01-28 17:10:33 +0100

    feat(storybook): add simple dark mode switch, load tokens from npm

--- packages/storybook-vue/.storybook/preview.js
+++ packages/storybook-vue/.storybook/preview.js
@@ -36,3 +36,22 @@ export const parameters = {
     viewports: INITIAL_VIEWPORTS, // newViewports would be an ViewportMap. (see below for examples)
   },
 };
+
+// Dark mode (punk) switch
+
+document.body.dataset.mode = 'light'
+
+setTimeout(() => {
+  console.log('To enable dark mode press Ctrl+Shift+M while having focus on the canvas')
+}, 3000)
+
+function switchMode(bool) {
+  document.body.dataset.mode = bool ? 'dark' : 'light'
+  console.log(`${document.body.dataset.mode.toUpperCase()} mode enabled`)
+}
+
+document.addEventListener('keydown', (event) => {
+  if (event.key.toUpperCase() === 'M' && event.ctrlKey && event.shiftKey) {
+    switchMode(document.body.dataset.mode === 'light')
+  }
+})
