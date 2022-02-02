e6c904d003684806983bf3d2a21985562113db98
kuro <sergey.grebenyukov@telekom.com>
2022-02-03 00:31:14 +0300

    docs: grid and grid-item stories (unfinished)

---
+++ packages/storybook-vue/stories/3_components/grid-item/ScaleGridItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <scale-grid-item
+    :size="size"
+    :offset="offset"
+  >
+    <slot></slot>
+  </scale-grid-item>
+</template>
+
+<script>
+export default {
+  props: {
+    size: String,
+    offset: String,
+  },
+};
+</script>
---
+++ packages/storybook-vue/stories/3_components/grid/ScaleGrid.vue
@@ -0,0 +1,23 @@
+<template>
+  <scale-grid
+    :columns="columns"
+    :gutter-x="gutterX"
+    :gutter-y="gutterY"
+    :spacing="spacing"
+    :max-width="maxWidth"
+  >
+  </scale-grid>
+</template>
+
+<script>
+
+export default {
+  props: {
+    columns: String,
+    gutterY: String,
+    gutterX: String,
+    spacing: String,
+    maxWidth: String,
+  },
+};
+</script>
