9327af2b0dd4b06362af97380303c8fa1cb4dbdd
Maomao <maomao.zha@gmail.com>
2022-02-03 14:44:18 +0200

    feat(data-table): add email and phone cells (#808)
    * feat: add email and telephone  data type to data grid cell
    
    * feat: add email and telephone data cell stories
    
    * chore: update dummy email and telephone number in story book
    
    * test: add email cell and phone cell spec tests
    
    Co-authored-by: Maomao Meyer-Zhang <maomao.meyer-zhang@telekom.de>

---
+++ packages/components/src/components/data-grid/cell-handlers/telephone-cell.tsx
@@ -0,0 +1,26 @@
+/**
+ * @license
+ * Scale https://github.com/telekom/scale
+ *
+ * Copyright (c) 2021 Egor Kirpichev and contributors, Deutsche Telekom AG
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+import { h } from '@stencil/core';
+import { Cell } from './cell-interface';
+
+// Expected content: a telephone number  string (eg: 'tel:+491234567')
+
+export const TelephoneCell: Cell = {
+  defaults: {
+    sortBy: 'text',
+  },
+  render: ({ content }) => {
+    // Remove protocol (tell:)
+    const telephoneNoProtocol = content.replace(/^tel:/i, '');
+    return <scale-link href={content}>{telephoneNoProtocol}</scale-link>;
+  },
+};
