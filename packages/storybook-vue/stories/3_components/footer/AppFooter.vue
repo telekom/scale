86bd6c77bf417a36794af3fd63d32eed6a952e50
Daniel Beck <daniel@beck.win>
2022-03-02 15:39:17 +0100

    docs(logo): update props

--- packages/storybook-vue/stories/3_components/footer/AppFooter.vue
+++ packages/storybook-vue/stories/3_components/footer/AppFooter.vue
@@ -3,6 +3,10 @@
     :footer-navigation="footerNavigation"
     :variant="variant"
     :copyright="copyright"
+    :logo-title="logoTitle"
+    :logo-href="logoHref"
+    :logo-aria-described-by="logoAriaDescribedBy"
+    @logo-click="logoClick"
   >
     <slot name="logo"></slot>
     <slot name="navigation"></slot>
@@ -15,6 +19,10 @@ export default {
     footerNavigation: String,
     variant: String,
     copyright: String,
-  },
+    logoTitle: String,
+    logoHref: String,
+    logoAriaDescribedBy: String,
+    logoClick: Function,
+  }
 };
 </script>
