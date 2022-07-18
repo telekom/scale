4408ce824a9aa8c22cb86aa20d3f1c3b7ea45cd1
marvinLaubenstein <marvin.laubenstein@telekom.de>
2022-07-18 17:26:34 +0200

    refactor: update table visual tests

--- packages/visual-tests/src/table.visual.spec.js
+++ packages/visual-tests/src/table.visual.spec.js
@@ -8,30 +8,28 @@ describe('Table', () => {
         localStorage.setItem('persistedColorMode', JSON.stringify(mode));
       }, mode);
     });
-    test.each([
-      ['standard'],
-      ['small'],
-      ['with-sorting-icons'],
-      ['with-striped-rows'],
-    ])('%p', async (variant) => {
-      await page.goto(
-        `http://host.docker.internal:3123/iframe.html?id=components-table--${variant}&viewMode=story`
-      );
-      await page.waitForSelector('html.hydrated');
-      await page.waitFor(5000);
-      const previewHtml = await page.$('body');
-      await page.evaluate(() => {
-        [
-          '--telekom-motion-duration-immediate',
-          '--telekom-motion-duration-transition',
-          '--telekom-motion-duration-animation',
-          '--telekom-motion-duration-animation-deliberate',
-        ].forEach((transitionSpeed) => {
-          document.body.style.setProperty(transitionSpeed, '0s');
+    test.each([['standard'], ['with-sorting-icons'], ['with-striped-rows']])(
+      '%p',
+      async (variant) => {
+        await page.goto(
+          `http://host.docker.internal:3123/iframe.html?id=components-table--${variant}&viewMode=story`
+        );
+        await page.waitForSelector('html.hydrated');
+        await page.waitFor(5000);
+        const previewHtml = await page.$('body');
+        await page.evaluate(() => {
+          [
+            '--telekom-motion-duration-immediate',
+            '--telekom-motion-duration-transition',
+            '--telekom-motion-duration-animation',
+            '--telekom-motion-duration-animation-deliberate',
+          ].forEach((transitionSpeed) => {
+            document.body.style.setProperty(transitionSpeed, '0s');
+          });
         });
-      });
-      expect(await previewHtml.screenshot()).toMatchImageSnapshot();
-    });
+        expect(await previewHtml.screenshot()).toMatchImageSnapshot();
+      }
+    );
     test.each([['standard']])('%p', async (variant) => {
       await page.goto(
         `http://host.docker.internal:3123/iframe.html?id=components-table--${variant}&viewMode=story`
