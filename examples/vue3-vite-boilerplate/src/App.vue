f7017eef50edfcdb4fa74a3a295c2b605a807829
Felix Weber <felix.weber@iconstorm.de>
2022-08-09 15:56:38 +0200

    fix: remove unnecessary code, use neutral components, update content

--- examples/vue3-vite-boilerplate/src/App.vue
+++ examples/vue3-vite-boilerplate/src/App.vue
@@ -1,14 +1,13 @@
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <h1 class="scl-font-variant-heading-1">Scale + Vue3 + Vite </h1>
+    <scale-button
+      href="https://telekom.github.io/scale/?path=/story/scale-for-developers-scale-and-angular--page"
+      title="External link"
+    >
+      Open the docs
+    </scale-button>
   </div>
-  <scale-button>Hello Scale</scale-button>
-  <span> hello </span>
 </template>
 
 <style>
@@ -23,6 +22,5 @@
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
-@import "@telekom/scale-components/dist/scale-components/scale-components.css";
 
 </style>
--- examples/vue3-vite-boilerplate/src/main.js
+++ examples/vue3-vite-boilerplate/src/main.js
@@ -1,8 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import {defineCustomElements} from "@telekom/scale-components/loader"
-import '@telekom/scale-components/dist/scale-components/scale-components.css';
+import {defineCustomElements} from "@telekom/scale-components-neutral/loader"
+import '@telekom/scale-components-neutral/dist/scale-components/scale-components.css';
 
 const app = createApp(App);
 app.mount('#app')
