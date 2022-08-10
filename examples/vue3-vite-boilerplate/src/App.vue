4d45f5b8fda25068b821dabf48b7387b83f61792
felix-ico <97440128+felix-ico@users.noreply.github.com>
2022-08-10 11:23:59 +0200

    docs: add vue3 vite boilerplate (#1160)
    * docs: add vue3 vite boilerplate
    
    * fix: remove unnecessary code, use neutral components, update content
    
    * fix: update link
    
    * fix: vanilla example

---
+++ examples/vue3-vite-boilerplate/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1 class="scl-font-variant-heading-1">Scale + Vue3 + Vite </h1>
+    <scale-button
+      href="https://telekom.github.io/scale/?path=/docs/setup-info-scale-and-vue--page"
+      title="External link"
+    >
+      Open the docs
+    </scale-button>
+  </div>
+</template>
+
+<style>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+
+</style>
