7193688a6ffaa75b3c7c575482400bc31c683059
Daniel Beck <daniel@beck.win>
2022-09-01 12:26:10 +0200

    test(card): visual

--- packages/visual-tests/src/card.visual.spec.js
+++ packages/visual-tests/src/card.visual.spec.js
@@ -11,8 +11,6 @@ describe('Card', () => {
       ['full-width-image-and-headline'],
       ['asset-headline-and-subline'],
       ['centered-image-and-headline'],
-      ['with-link'],
-      ['with-further-functions'],
     ])('%p', async (variant) => {
       await global.runSetup(`components-card--${variant}`);
       await global.visualCheck();
