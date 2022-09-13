8fb5969778824252fddc5c3c2df7c3e6ff4e53b5
Daniel Beck <daniel@beck.win>
2022-09-13 17:36:53 +0300

    feat(interactive-icon): add new component

---
+++ packages/components/src/components/interactive-icon/interactive-icon.e2e.ts
@@ -0,0 +1,32 @@
+/**
+ * @license
+ * Scale https://github.com/telekom/scale
+ *
+ * Copyright (c) 2021 Egor Kirpichev and contributors, Deutsche Telekom AG
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+import { newE2EPage } from '@stencil/core/testing';
+
+describe('scale-interactive-icon', () => {
+  it('renders', async () => {
+    const page = await newE2EPage();
+    await page.setContent(
+      '<scale-interactive-icon><scale-icon-action-search></scale-icon-action-search></scale-interactive-icon>'
+    );
+    const element = await page.find('scale-interactive-icon');
+    expect(element).toHaveClass('hydrated');
+  });
+
+  it('should set tabindex on its inner element', async () => {
+    const page = await newE2EPage();
+    await page.setContent(
+      '<scale-interactive-icon inner-tabindex="5"><scale-icon-action-search></scale-icon-action-search></scale-interactive-icon>'
+    );
+    const element = await page.find('scale-interactive-icon >>> button');
+    expect(element.getAttribute('tabindex')).toBe('5');
+  });
+});
---
+++ packages/storybook-vue/stories/components/interactive-icon/ScaleInteractiveIcon.vue
@@ -0,0 +1,28 @@
+<template>
+  <scale-interactive-icon
+    :size="size"
+    :active="active"
+    :innerTabindex="innerTabindex"
+    :download="download"
+    :value="value"
+    :name="name"
+    :type="type"
+  >
+    <slot></slot>
+  </scale-interactive-icon>
+</template>
+
+<script>
+
+export default {
+  props: {
+    size: { type: String },
+    type: { type: String },
+    name: { type: String },
+    value: { type: String },
+    download: { type: String },
+    innerTabindex: { type: String },
+    active: { type: String },
+  },
+};
+</script>
