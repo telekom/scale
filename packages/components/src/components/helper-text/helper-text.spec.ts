79580a7b36f7c75c61d2140c33c87fa2f7c300ef
Felix Weber <felix.weber@iconstorm.de>
2022-09-26 14:42:04 +0200

    fix: format

--- packages/components/src/components/helper-text/helper-text.spec.ts
+++ packages/components/src/components/helper-text/helper-text.spec.ts
@@ -9,61 +9,60 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
- import { newSpecPage } from '@stencil/core/testing';
- import { HelperText } from './helper-text';
- 
- describe('Helper Text', () => {
-   let element;
-   beforeEach(async () => {
-     element = new HelperText();
-   });
- 
-   it('should render informational helper text with info icon if no variant is specified', async () => {
-     const page = await newSpecPage({
-       components: [HelperText],
-       html: `<scale-helper-text>informational</scale-helper-text>`,
-     });
-     expect(page.root).toMatchSnapshot();
-   });
+import { newSpecPage } from '@stencil/core/testing';
+import { HelperText } from './helper-text';
 
-   it('should render informational helper text with info icon if informational variant is specified', async () => {
+describe('Helper Text', () => {
+  let element;
+  beforeEach(async () => {
+    element = new HelperText();
+  });
+
+  it('should render informational helper text with info icon if no variant is specified', async () => {
+    const page = await newSpecPage({
+      components: [HelperText],
+      html: `<scale-helper-text>informational</scale-helper-text>`,
+    });
+    expect(page.root).toMatchSnapshot();
+  });
+
+  it('should render informational helper text with info icon if informational variant is specified', async () => {
     const page = await newSpecPage({
       components: [HelperText],
       html: `<scale-helper-text variant="informational">informational</scale-helper-text>`,
     });
     expect(page.root).toMatchSnapshot();
-  });   
- 
-   it('should render warning text with warning icon if warning variant is specified', async () => {
-     const page = await newSpecPage({
-       components: [HelperText],
-       html: `<scale-helper-text variant="warning">warning</scale-helper-text>`,
-     });
-     expect(page.root).toMatchSnapshot();
-   });
- 
-   it('should render danger text with danger icon if dange variant is specified', async () => {
-     const page = await newSpecPage({
-       components: [HelperText],
-       html: `<scale-helper-text variant="danger">danger</scale-helper-text>`,
-     });
-     expect(page.root).toMatchSnapshot();
-   });
- 
-   it('should render success text with success icon if success variant is specified', async () => {
-     const page = await newSpecPage({
-       components: [HelperText],
-       html: `<scale-helper-text variant="success">success</scale-helper-text>`,
-     });
-     expect(page.root).toMatchSnapshot();
-   });
- 
-   it('should render neutral text without icon if neutral variant is specified', async () => {
-     const page = await newSpecPage({
-       components: [HelperText],
-       html: `<scale-helper-text variant="neutral">neutral</scale-helper-text>`,
-     });
-     expect(page.root).toMatchSnapshot();
-   });
- });
- 
+  });
+
+  it('should render warning text with warning icon if warning variant is specified', async () => {
+    const page = await newSpecPage({
+      components: [HelperText],
+      html: `<scale-helper-text variant="warning">warning</scale-helper-text>`,
+    });
+    expect(page.root).toMatchSnapshot();
+  });
+
+  it('should render danger text with danger icon if dange variant is specified', async () => {
+    const page = await newSpecPage({
+      components: [HelperText],
+      html: `<scale-helper-text variant="danger">danger</scale-helper-text>`,
+    });
+    expect(page.root).toMatchSnapshot();
+  });
+
+  it('should render success text with success icon if success variant is specified', async () => {
+    const page = await newSpecPage({
+      components: [HelperText],
+      html: `<scale-helper-text variant="success">success</scale-helper-text>`,
+    });
+    expect(page.root).toMatchSnapshot();
+  });
+
+  it('should render neutral text without icon if neutral variant is specified', async () => {
+    const page = await newSpecPage({
+      components: [HelperText],
+      html: `<scale-helper-text variant="neutral">neutral</scale-helper-text>`,
+    });
+    expect(page.root).toMatchSnapshot();
+  });
+});
