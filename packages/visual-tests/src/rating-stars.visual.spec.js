c790ce7fc9cd44029aee1a4d2e49a6edf6077069
Daniel Beck <daniel@beck.win>
2022-10-26 18:32:02 +0200

    test: bust flaky rating stars test

--- packages/visual-tests/src/rating-stars.visual.spec.js
+++ packages/visual-tests/src/rating-stars.visual.spec.js
@@ -9,21 +9,17 @@ describe('RatingStars', () => {
       ['small-with-info-text'],
       ['hidden-label'],
       ['readonly'],
-    ])('%p', async (variant) => {
+    ])('story %p', async (variant) => {
       await global.runSetup(`components-rating-stars--${variant}`);
       await global.visualCheck();
     });
     // focus, active
-    test.each([['info-text-and-custom-label']])('%p', async (variant) => {
+    test.each([['info-text-and-custom-label']])('focus %p', async (variant) => {
       await global.runSetup(`components-rating-stars--${variant}`);
       const input = await global.page.evaluateHandle(
         `document.querySelector("#root > scale-rating-stars").shadowRoot.querySelector("input[type=range]")`
       );
       await input.focus();
-      await global.visualCheck();
-      await global.page.mouse.move(40, 60);
-      await global.page.mouse.down();
-      await global.page.waitFor(600);
       await global.visualCheck();
     });
   });
