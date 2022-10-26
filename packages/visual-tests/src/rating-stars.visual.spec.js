c21156b7c98d133f6973cac05f4d196e341094ac
Daniel Beck <daniel@beck.win>
2022-10-26 21:58:22 +0200

    test: bust flaky rating stars test #2

--- packages/visual-tests/src/rating-stars.visual.spec.js
+++ packages/visual-tests/src/rating-stars.visual.spec.js
@@ -13,14 +13,27 @@ describe('RatingStars', () => {
       await global.runSetup(`components-rating-stars--${variant}`);
       await global.visualCheck();
     });
-    // focus, active
     test.each([['info-text-and-custom-label']])('focus %p', async (variant) => {
       await global.runSetup(`components-rating-stars--${variant}`);
       const input = await global.page.evaluateHandle(
         `document.querySelector("#root > scale-rating-stars").shadowRoot.querySelector("input[type=range]")`
       );
       await input.focus();
+      await global.page.waitFor(600);
       await global.visualCheck();
     });
+    test.each([['info-text-and-custom-label']])(
+      'active %p',
+      async (variant) => {
+        await global.runSetup(`components-rating-stars--${variant}`);
+        const input = await global.page.evaluateHandle(
+          `document.querySelector("#root > scale-rating-stars").shadowRoot.querySelector("input[type=range]")`
+        );
+        await global.page.mouse.move(40, 60);
+        await global.page.mouse.down();
+        await global.page.waitFor(600);
+        await global.visualCheck();
+      }
+    );
   });
 });
