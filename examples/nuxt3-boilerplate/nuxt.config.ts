217e2a4af4f012a312d6916a7fb049e4903fbadc
Felix Weber <felix.weber@iconstorm.de>
2022-11-18 10:15:08 +0100

    chore: use neutral compononets

--- examples/nuxt3-boilerplate/nuxt.config.ts
+++ examples/nuxt3-boilerplate/nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   build: {
-    transpile: ['@telekom/scale-components']
+    transpile: ['@telekom/scale-components-neutral']
   },  
   vue: {
     compilerOptions: {
