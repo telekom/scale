6a78fa03e9915160f0f2edec38c264190a910c8e
Felix Weber <felix.weber@iconstorm.de>
2022-11-24 10:15:52 +0100

    feat: improve mobile preview positioning

--- packages/storybook-vue/.storybook/mobile-preview/register.js
+++ packages/storybook-vue/.storybook/mobile-preview/register.js
@@ -13,17 +13,21 @@ addons.register('@telekom/scale-mobile-prpeview', (api) => {
                 document.querySelectorAll('#storybook-preview-iframe').forEach( item =>
                     {
                             const storyCanvas = item.contentWindow.document.body.querySelectorAll('.docs-story')
-                            // item.style = 'width: 762px; border: none'
                             storyCanvas.forEach(el => {
                                 const firstChild = el.firstChild;
-                                firstChild.style = "padding: 0; height: 100%;"
+                                firstChild.style = "padding: 0; height: 100%; margin: 0;"
+                                const wrapper = el.querySelector('.innerZoomElementWrapper');
+                                wrapper.firstChild.style = "border: none !important;"
+
+                                const rootEl = el.querySelector('#root').firstChild
+                                rootEl.style = "position: relative; height: 612px;"
+
                                 const parentNode = el.parentNode;
                                 let topEl = document.createElement("div");
                                 topEl.classList.add("top-frame")
                                 let bottomEl = document.createElement("div");
                                 bottomEl.classList.add("bottom-frame")
                                 parentNode.style = "width: 375px; border-radius: 45px; filter: drop-shadow(0px 6px 16px rgba(0, 0, 0, 0.15)) drop-shadow(0px 12px 48px rgba(0, 0, 0, 0.1));"
-                                el.style = "height: 608px;"
                                 topEl.style = "height: 77px; background-color: #FBFBFB; border-radius: 45px 45px 0 0;"
                                 bottomEl.style = "height: 77px; background-color: #FBFBFB; border-radius: 0 0 45px 45px;"
                                 const topFrame = parentNode.querySelector('.top-frame')
@@ -31,7 +35,7 @@ addons.register('@telekom/scale-mobile-prpeview', (api) => {
                                     parentNode.insertBefore(topEl, el)
                                     parentNode.insertBefore(bottomEl, null)
                                 }
-                                el.style = "border: 1px solid #E7E7E9; height: 608px;"
+                                el.style = "border: 1px solid #E7E7E9; height: 612px;"
                             })
                     }
                 )
