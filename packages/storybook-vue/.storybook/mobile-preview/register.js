bea87ef6f02bdac8a168dd808e04656531e8be99
Felix Weber <felix.weber@iconstorm.de>
2022-12-01 16:12:18 +0100

    fix: shadows

--- packages/storybook-vue/.storybook/mobile-preview/register.js
+++ packages/storybook-vue/.storybook/mobile-preview/register.js
@@ -25,7 +25,7 @@ addons.register('@telekom/scale-mobile-prpeview', (api) => {
                                 topEl.classList.add("top-frame")
                                 let bottomEl = document.createElement("div");
                                 bottomEl.classList.add("bottom-frame")
-                                parentNode.style = "width: 375px; border-radius: 45px; filter: drop-shadow(0px 6px 16px rgba(0, 0, 0, 0.15)) drop-shadow(0px 12px 48px rgba(0, 0, 0, 0.1));"
+                                parentNode.style = "width: 375px; border-radius: 45px; box-shadow: 0px 6px 16px rgba(0, 0, 0, 0.15), 0px 12px 48px rgba(0, 0, 0, 0.1);"
                                 topEl.style = "height: 77px; background-color: #FBFBFB; border-radius: 45px 45px 0 0;"
                                 bottomEl.style = "height: 77px; background-color: #FBFBFB; border-radius: 0 0 45px 45px;"
                                 const topFrame = parentNode.querySelector('.top-frame')
