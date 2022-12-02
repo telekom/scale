24f064dfec642c2fed62f64850d9436cf8d2a31d
niclas18 <87126599+niclas18@users.noreply.github.com>
2022-12-02 12:32:05 +0100

    feat(text-field): add autocomplete property (#1400)
    * add autocomplete-prop to textField
    
    * add space in jsdoc for passing checks
    
    * add readme because of property-change
    
    * rename autocomplete to inputAutocomplete and add it in the storybook
    
    Co-authored-by: nhoralek <niclas.horalek@telekom.de>

--- packages/components/src/components/text-field/text-field.tsx
+++ packages/components/src/components/text-field/text-field.tsx
@@ -105,6 +105,8 @@ export class TextField {
   @Prop() list?: string;
   /** (optional) the input should automatically get focus when the page loads. */
   @Prop() inputAutofocus?: boolean;
+  /** (optional) custom value for autocomplete HTML attribute */
+  @Prop() inputAutocomplete?: string;
 
   /** (optional) Injected CSS styles */
   @Prop() styles?: string;
@@ -266,6 +268,7 @@ export class TextField {
             {...(!!this.placeholder ? { placeholder: this.placeholder } : {})}
             disabled={this.disabled}
             readonly={this.readonly}
+            autocomplete={this.inputAutocomplete}
             {...ariaInvalidAttr}
             {...(this.helperText ? ariaDescribedByAttr : {})}
             {...(numericTypes.includes(this.type) ? { step: this.step } : {})}
