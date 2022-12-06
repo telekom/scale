c9056ced7bb6bcb0a5e38721124a6a658cf84779
Daniel Beck <daniel@beck.win>
2022-12-06 10:46:49 +0100

    docs(telekom-mobile-menu): add story

---
+++ packages/storybook-vue/stories/components/beta-brand-header-navigation/TelekomHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <scale-telekom-header
+    :app-name-click="appNameClick"
+    :app-name-title="appNameTitle"
+    :app-name="appName"
+  >
+    <slot name="meta-nav-ext"></slot>
+    <slot name="meta-nav"></slot>
+    <slot name="language-switch"></slot>
+    <slot name="main-nav"></slot>
+    <slot name="functions"></slot>
+  </scale-telekom-header>
+</template>
+
+<script>
+export default {
+  props: {
+    appNameClick: Function,
+    appNameTitle: String,
+    appName: String,
+  },
+};
+</script>
