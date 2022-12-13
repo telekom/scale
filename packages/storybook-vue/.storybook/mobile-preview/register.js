cb4f51748ba9858f11ead0bbc48906994760dada
Felix Weber <felix.weber@iconstorm.de>
2022-12-13 10:23:15 +0100

    feat: update styles

--- packages/storybook-vue/.storybook/mobile-preview/register.js
+++ packages/storybook-vue/.storybook/mobile-preview/register.js
@@ -13,6 +13,7 @@ addons.register('@telekom/scale-mobile-prpeview', (api) => {
                     {   
                             const canvasHeight = mobilePreview == 'full' ? '612px' : '306px';
                             const canvasBorderRadius = mobilePreview == 'full' ? '45px' : mobilePreview == 'bottom' ? '0 0 45px 45px' : '45px 45px 0 0'; 
+
                             const storyCanvas = item.contentWindow.document.body.querySelectorAll('.docs-story')
                             storyCanvas.forEach(el => {
                                 const firstChild = el.firstChild;
@@ -22,24 +23,31 @@ addons.register('@telekom/scale-mobile-prpeview', (api) => {
                                 const rootEl = el.querySelector('#root')
                                 rootEl.style = `position: relative; height: ${canvasHeight};`
                                 const parentNode = el.parentNode;
-                                let topEl = document.createElement("div");
-                                topEl.classList.add("top-frame")
-                                let bottomEl = document.createElement("div");
-                                bottomEl.classList.add("bottom-frame")
-                                parentNode.style = `width: 375px; border-radius: ${canvasBorderRadius}; box-shadow: 0px 6px 16px rgba(0, 0, 0, 0.15), 0px 12px 48px rgba(0, 0, 0, 0.1);`
-                                topEl.style = "height: 77px; background-color: #FBFBFB; border-radius: 45px 45px 0 0;"
-                                bottomEl.style = "height: 77px; background-color: #FBFBFB; border-radius: 0 0 45px 45px;"
-                                const topFrame = parentNode.querySelector('.top-frame')
-                                const bottomFrame = parentNode.querySelector('.bottom-frame')
-                                if (mobilePreview == 'full' && topFrame == null ) {
-                                    parentNode.insertBefore(topEl, el)
-                                    parentNode.insertBefore(bottomEl, null)
-                                } else if (mobilePreview == 'bottom' && bottomFrame == null) {
-                                    parentNode.insertBefore(bottomEl, null)
-                                } else if (mobilePreview == 'top' && topFrame == null) {
-                                    parentNode.insertBefore(topEl, el)
+
+                                var parentWrapper = document.createElement('div');
+                                parentWrapper.classList.add('parentWrapper')
+                                parentWrapper.style = "width: 100%; display: flex; justify-content: center; align-items: center; background-color: #F7F7F8;"
+                                parentNode.parentNode.replaceChild(parentWrapper, parentNode)
+                                parentWrapper.appendChild(parentNode)
+                                parentNode.style = `width: 375px; border-radius: ${canvasBorderRadius}; box-shadow: var(--telekom-shadow-floating-standard); ${mobilePreview == 'top' ? 'margin-bottom: 0;' : mobilePreview =='bottom' ? 'margin-top: 0;' : '' } border: none;`
+           
+                                let topFrame = document.createElement("div");
+                                topFrame.classList.add("top-frame")
+                                let bottomFrame = document.createElement("div");
+                                bottomFrame.classList.add("bottom-frame")
+                                topFrame.style = "height: 77px; background-color: #FBFBFB; border-radius: 45px 45px 0 0;"
+                                bottomFrame.style = "height: 77px; background-color: #FBFBFB; border-radius: 0 0 45px 45px;"
+                                const topFrameExists = parentNode.querySelector('.top-frame')
+                                const bottomFrameExists = parentNode.querySelector('.bottom-frame')
+                                if (mobilePreview == 'full' && topFrameExists == null ) {
+                                    parentNode.insertBefore(topFrame, el)
+                                    parentNode.insertBefore(bottomFrame, null)
+                                } else if (mobilePreview == 'bottom' && bottomFrameExists == null) {
+                                    parentNode.insertBefore(bottomFrame, null)
+                                } else if (mobilePreview == 'top' && topFrameExists == null) {
+                                    parentNode.insertBefore(topFrame, el)
                                 }
-                                el.style = `border: 1px solid #E7E7E9; height: ${canvasHeight};`
+                                el.style = `${mobilePreview == 'bottom' ? 'border-bottom: 1px solid #E7E7E9;' : mobilePreview == 'top' ? 'border-top: 1px solid #E7E7E9;' : 'border-bottom: 1px solid #E7E7E9; border-top: 1px solid #E7E7E9;' } height: ${canvasHeight};`
                             })
                     }
                 )
