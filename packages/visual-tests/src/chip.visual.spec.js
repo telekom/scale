54993ac85963d4f26b71a160d4368414294f26ec
Marvin_Laubenstein <82942834+marvinLaubenstein@users.noreply.github.com>
2022-12-21 13:37:04 +0100

    refactor: changing stories label

--- packages/visual-tests/src/chip.visual.spec.js
+++ packages/visual-tests/src/chip.visual.spec.js
@@ -12,35 +12,10 @@ describe('Chip', () => {
       ['outline-selected'],
       ['outline-selected-disabled'],
       ['outline-dismissible'],
-      ['icon'],
+      ['icon+text'],
     ])('%p', async (variant) => {
       await global.runSetup(`beta-components-chip--${variant}`);
       await global.visualCheck();
-    });
-    test.each([
-      ['standard', 'hover'],
-      ['standard', 'active'],
-      ['standard', 'focus'],
-    ])('%p', async (variant, state) => {
-      await global.runSetup(`beta-components-chip--${variant}`);
-      await global.page.waitForSelector('html.hydrated');
-      await global.page.$('body');
-      const checkbox = await global.page.evaluateHandle(
-        `document.querySelector("#root > scale-chip")`
-      );
-      if (state === 'hover') {
-        await checkbox.hover();
-        await global.visualCheck();
-      }
-      if (state === 'active') {
-        await global.page.mouse.move(20, 20);
-        await global.page.mouse.down();
-        await global.visualCheck();
-      }
-      if (state === 'focus') {
-        await checkbox.focus();
-        await global.visualCheck();
-      }
     });
   });
 });
