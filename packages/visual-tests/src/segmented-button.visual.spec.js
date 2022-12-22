ca236ad6924c1e76c2eb9d5a234a2c7c66147727
Marvin_Laubenstein <82942834+marvinLaubenstein@users.noreply.github.com>
2022-12-22 18:10:29 +0100

    feat: adding segmented button

---
+++ packages/visual-tests/src/segmented-button.visual.spec.js
@@ -0,0 +1,19 @@
+describe('SegmentedButton', () => {
+  describe.each(['light', 'dark'])('%p', (mode) => {
+    beforeAll(async () => {
+      await global.runColorSetup('components-segmented-button--standard', mode);
+    });
+    test.each([
+      ['standard'],
+      ['multi-select'],
+      ['disabled-button'],
+      ['disabled-button-group'],
+      ['icon-only'],
+      ['icon-and-text'],
+      ['invalid'],
+    ])('%p', async (variant) => {
+      await global.runSetup(`beta-components-segmented-button--${variant}`);
+      await global.visualCheck();
+    });
+  });
+});
