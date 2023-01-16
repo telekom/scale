a9ba29081c4b51583cb8de6dd28cfbfe58a4cc3e
Felix Weber <felix.weber@iconstorm.de>
2023-01-16 16:16:14 +0100

    fix: update styles

--- packages/storybook-vue/utils/mobilePreviewDecorator.jsx
+++ packages/storybook-vue/utils/mobilePreviewDecorator.jsx
@@ -4,16 +4,18 @@ const mobilePreviewDecorator = (story, context) => {
     const canvasHeight = mobilePreview == 'full' ? '612px' : '306px';
     const canvasBorderRadius = mobilePreview == 'full' ? '45px' : mobilePreview == 'bottom' ? '0 0 45px 45px' : '45px 45px 0 0';
     const spacing = mobilePreview == 'full' ? '40px 0 40px 0' : mobilePreview == 'bottom' ? '0 0 40px 0' : '40px 0 0 0';
-    if (viewMode === 'docs') {
+    if (viewMode === 'docs') {  
         return ({
         template: `
-            <div style="width: 100%; display: flex; justify-content: center; align-items: center; background-color: #F7F7F8; border: 1px solid rgba(255, 255, 255, 0.4);">
-                <div class="mobileContainer" style="border: 1px solid rgba(255, 255, 255, 0.4); margin: ${spacing}">
+            <div style="width: 100%; display: flex; justify-content: center; align-items: center; background-color: #F7F7F8; border: 1px solid #e7e7e9;">
+                <div class="mobileContainer" style="margin: ${spacing}">
                     <div class="foobar" style="width: 375px; border-radius: ${canvasBorderRadius}; box-shadow: var(--telekom-shadow-floating-standard);">
+                        
+                        ${mobilePreview === 'top' || mobilePreview === 'full' ? `<div style="height: 77px; background-color: #FBFBFB; border-radius: ${canvasBorderRadius}; border-bottom: 1px solid #e7e7e9;"></div>` : ''}
                         <div style="height:${canvasHeight}; background-color: #fff; position: relative;">
                             <story />
                         </div>
-                        <div style="height: 77px; background-color: #FBFBFB; border-radius: 0 0 45px 45px; border-top: 1px solid #e7e7e9;"></div>
+                        ${mobilePreview === 'bottom' || mobilePreview === 'full' ? `<div style="height: 77px; background-color: #FBFBFB; border-radius: ${canvasBorderRadius}; border-top: 1px solid #e7e7e9;"></div>` : ''}
                     </div>          
                 </div>
             </div>
