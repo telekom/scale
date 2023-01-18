e7ce1432ce7398152b30d3c40830a18dec4e3cdb
Felix Weber <felix.weber@iconstorm.de>
2023-01-18 12:51:44 +0100

    fix: format

--- packages/components/src/components/segmented-button/segmented-button.tsx
+++ packages/components/src/components/segmented-button/segmented-button.tsx
@@ -140,8 +140,8 @@ export class SegmentedButton {
         this.hostElement.children.length
       }, ${Math.ceil(longestButtonWidth)}px`;
     } else {
-      this.container.style.gridAutoColumns = "minmax(0, 1fr);";
-      this.container.style.gridAutoFlow = "column"
+      this.container.style.gridAutoColumns = 'minmax(0, 1fr);';
+      this.container.style.gridAutoFlow = 'column';
     }
 
     this.selectedIndex = this.getSelectedIndex();
