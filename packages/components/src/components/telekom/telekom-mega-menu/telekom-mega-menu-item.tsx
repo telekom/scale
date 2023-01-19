282026c48a498d1e866de0d8e77844838acfa8d7
felix-ico <97440128+felix-ico@users.noreply.github.com>
2023-01-19 16:56:00 +0100

    feat(telekom-mega-menu): add new components (#1491)
    * feat: desktop mega menu initial commit
    
    * style: format
    
    * style: lint
    
    * test(visual): update snapshots (#1492)
    
    Co-authored-by: felix-ico <felix-ico@users.noreply.github.com>
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: felix-ico <felix-ico@users.noreply.github.com>

---
+++ packages/components/src/components/telekom/telekom-mega-menu/telekom-mega-menu-item.tsx
@@ -0,0 +1,32 @@
+/**
+ * @license
+ * Scale https://github.com/telekom/scale
+ *
+ * Copyright (c) 2021 Egor Kirpichev and contributors, Deutsche Telekom AG
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+import { Component, h, Host, Element, Prop } from '@stencil/core';
+import { HTMLStencilElement } from '@stencil/core/internal';
+
+@Component({
+  tag: 'scale-telekom-mega-menu-item',
+  styleUrl: 'telekom-mega-menu-item.css',
+  shadow: true,
+})
+export class TelekomMegaMenuItem {
+  @Element() hostElement: HTMLStencilElement;
+
+  render() {
+    return (
+      <Host>
+        <div part="base">
+          <slot></slot>
+        </div>
+      </Host>
+    );
+  }
+}
---
+++ packages/components/src/components/telekom/telekom-mega-menu/telekom-mega-menu.tsx
@@ -0,0 +1,45 @@
+/**
+ * @license
+ * Scale https://github.com/telekom/scale
+ *
+ * Copyright (c) 2021 Egor Kirpichev and contributors, Deutsche Telekom AG
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+import { Component, h, Host, Element, Prop } from '@stencil/core';
+import { HTMLStencilElement } from '@stencil/core/internal';
+
+@Component({
+  tag: 'scale-telekom-mega-menu',
+  styleUrl: 'telekom-mega-menu.css',
+  shadow: true,
+})
+export class TelekomMegaMenu {
+  @Element() hostElement: HTMLStencilElement;
+  private container: HTMLElement;
+
+  componentDidLoad() {
+    const slotted = this.hostElement.children;
+    if (slotted.length < 5) {
+      this.container.style.paddingLeft = 'var(--spacing-4-columns)';
+    }
+  }
+
+  render() {
+    return (
+      <Host>
+        <div
+          ref={(el) => {
+            this.container = el;
+          }}
+          part="base"
+        >
+          <slot></slot>
+        </div>
+      </Host>
+    );
+  }
+}
