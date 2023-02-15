c8638b3670aa49fe0b83a457242b8089be57b7c9
Felix Weber <felixweber@MacBook-Pro-von-Administrator.local>
2023-02-15 14:26:11 +0100

    feat: update styling, add extended footer story

--- packages/storybook-vue/stories/components/beta-footer/Footer.vue
+++ packages/storybook-vue/stories/components/beta-footer/Footer.vue
@@ -1,38 +1,40 @@
 <template>
     <scale-telekom-footer :variant="variant">
-      <span slot="notice"> © Deutsche Telekom AG </span>
-      <ul slot="navigation">
-        <li>
-          <a href="#">
-            Impressum 
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            Datenschutz 
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            AGB
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            Produktinformationsblatt
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            Verbraucherinformationen
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            Jugendschutz
-          </a>
-        </li>
-      </ul>
+      <scale-telekom-footer-content>
+        <span slot="notice"> © Deutsche Telekom AG </span>
+        <ul slot="navigation">
+          <li>
+            <a href="#">
+              Impressum 
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              Datenschutz 
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              AGB
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              Produktinformationsblatt
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              Verbraucherinformationen
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              Jugendschutz
+            </a>
+          </li>
+        </ul>
+      </scale-telekom-footer-content>
     </scale-telekom-footer>
   </template>
   
