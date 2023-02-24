bb622917ac7c38975273c811609687e9f343a231
Daniel Beck <daniel@beck.win>
2023-02-24 14:35:56 +0100

    feat(footer-back-compat): add component

--- packages/storybook-vue/stories/components/beta-brand-header-navigation/fixtures.js
+++ packages/storybook-vue/stories/components/beta-brand-header-navigation/fixtures.js
@@ -363,27 +363,3 @@ export const sectorNavigation = [
   { name: 'Personal', id: 'Personal38', href: '#personal' },
   { name: 'Business', id: 'Business39', href: '#business' },
 ];
-
-export const footerNavigation = [
-  {
-    name: 'Contact',
-    id: 'Contact',
-    href: '#contact',
-  },
-  {
-    name: 'Terms and conditions',
-    id: 'Terms and conditions',
-    href: '#terms-and-conditions',
-  },
-  {
-    name: 'Legal notice',
-    id: 'Legal notice',
-    href: '#legal-notice',
-  },
-  {
-    name: 'Data privacy',
-    id: 'Data privacy',
-    href: '#data-privacy',
-    icon: 'alert-imprint-dataprivacy',
-  },
-];
