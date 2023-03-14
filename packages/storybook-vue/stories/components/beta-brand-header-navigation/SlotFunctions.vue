4a795e615a200f3598bd9eec238b36700de2175d
Daniel Beck <daniel@beck.win>
2023-03-14 16:01:48 +0100

    refactor(header): responsive functions

--- packages/storybook-vue/stories/components/beta-brand-header-navigation/SlotFunctions.vue
+++ packages/storybook-vue/stories/components/beta-brand-header-navigation/SlotFunctions.vue
@@ -1,25 +1,35 @@
 <template>
-  <scale-telekom-nav-list variant="functions" slot="functions" alignment="right">
-    <scale-telekom-nav-item>
+  <scale-telekom-nav-list
+    variant="functions"
+    slot="functions"
+    alignment="right"
+  >
+    <scale-telekom-nav-item search>
       <a href="#">
-        <scale-icon-action-search></scale-icon-action-search>
+        <scale-badge no-dot label="Search">
+          <scale-icon-action-search></scale-icon-action-search>
+        </scale-badge>
       </a>
     </scale-telekom-nav-item>
-    <scale-telekom-nav-item>
+    <scale-telekom-nav-item cc>
       <a href="#">
-        <scale-icon-content-credit-card></scale-icon-content-credit-card>
+        <scale-badge no-dot label="Payment" label-visually-hidden>
+          <scale-icon-content-credit-card></scale-icon-content-credit-card>
+        </scale-badge>
       </a>
     </scale-telekom-nav-item>
-    <scale-telekom-nav-item>
+    <scale-telekom-nav-item cart>
       <a href="#">
-        <scale-badge count="1" label="Cart" label-visually-hidden>
+        <scale-badge count="1" label="Cart">
           <scale-icon-action-shopping-cart></scale-icon-action-shopping-cart>
         </scale-badge>
       </a>
     </scale-telekom-nav-item>
-    <scale-telekom-nav-item>
+    <scale-telekom-nav-item user>
       <a href="#">
-        <scale-icon-user-file-user></scale-icon-user-file-user>
+        <scale-badge no-dot label="Login">
+          <scale-icon-user-file-user></scale-icon-user-file-user>
+        </scale-badge>
       </a>
     </scale-telekom-nav-item>
     <scale-telekom-nav-item class="burger-item">
@@ -29,7 +39,10 @@
         </scale-badge>
       </button>
       <scale-telekom-nav-flyout class="mobile-nav-flyout">
-        <SlotMobileFlyoutCanvas :app-name="appName" :app-name-link="appNameLink" />
+        <SlotMobileFlyoutCanvas
+          :app-name="appName"
+          :app-name-link="appNameLink"
+        />
       </scale-telekom-nav-flyout>
     </scale-telekom-nav-item>
   </scale-telekom-nav-list>
@@ -43,7 +56,7 @@ export default {
   props: {
     appName: String,
     appNameLink: String,
-  }
+  },
 };
 </script>
 
@@ -55,8 +68,25 @@ export default {
   --background: var(--telekom-color-background-canvas);
 }
 
+[search],
+[cart],
+[user] {
+  display: none;
+}
+
 @media screen and (min-width: 1040px) {
   .burger-item {
+    display: none;
+  }
+}
+
+@media screen and (min-width: 1040px) {
+  [search],
+  [cart],
+  [user] {
+    display: block;
+  }
+  [cc] {
     display: none;
   }
 }
