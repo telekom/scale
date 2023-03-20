3b75802a247cd4fc228227b75bb4313fc2051e5a
Daniel Beck <daniel@beck.win>
2023-03-20 17:27:43 +0100

    chore: release prep (#1676)
    * chore: release prep
    
    * chore: improve slim header xxl spacing
    
    * chore: docs
    
    * chore: coming soon text
    
    * chore: header functions
    
    * chore: header story name

--- packages/storybook-vue/stories/components/beta-brand-header-navigation/SlotFunctions.vue
+++ packages/storybook-vue/stories/components/beta-brand-header-navigation/SlotFunctions.vue
@@ -4,28 +4,21 @@
     slot="functions"
     alignment="right"
   >
-    <scale-telekom-nav-item search>
+    <scale-telekom-nav-item>
       <a href="#">
         <scale-badge no-dot label="Search">
           <scale-icon-action-search></scale-icon-action-search>
         </scale-badge>
       </a>
     </scale-telekom-nav-item>
-    <scale-telekom-nav-item cc>
-      <a href="#">
-        <scale-badge no-dot label="Payment" label-visually-hidden>
-          <scale-icon-content-credit-card></scale-icon-content-credit-card>
-        </scale-badge>
-      </a>
-    </scale-telekom-nav-item>
-    <scale-telekom-nav-item cart>
+    <scale-telekom-nav-item>
       <a href="#">
         <scale-badge count="1" label="Cart">
           <scale-icon-action-shopping-cart></scale-icon-action-shopping-cart>
         </scale-badge>
       </a>
     </scale-telekom-nav-item>
-    <scale-telekom-nav-item user>
+    <scale-telekom-nav-item>
       <a href="#">
         <scale-badge no-dot label="Login">
           <scale-icon-user-file-user></scale-icon-user-file-user>
@@ -68,9 +61,7 @@ export default {
   --background: var(--telekom-color-background-canvas);
 }
 
-[search],
-[cart],
-[user] {
+scale-badge::part(label) {
   display: none;
 }
 
@@ -81,13 +72,8 @@ export default {
 }
 
 @media screen and (min-width: 1040px) {
-  [search],
-  [cart],
-  [user] {
-    display: block;
-  }
-  [cc] {
-    display: none;
+  scale-badge::part(label) {
+    display: flex;
   }
 }
 </style>
