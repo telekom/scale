3ab678b03b76e735708291a75fbe775a90a94455
ayeshaa-m <ayesha.mulla@t-systems.com>
2023-03-24 17:10:41 +0530

    fix: reset pagination to the last page of the new records if new records are less than previous

--- packages/components/src/components/data-grid/data-grid.tsx
+++ packages/components/src/components/data-grid/data-grid.tsx
@@ -201,8 +201,10 @@ export class DataGrid {
   }
   @Watch('rows')
   rowsHandler() {
-    // Reset pagination start point to render new data.
-    this.paginationStart = 0;
+    // Reset pagination to the last page of the new records if new records are less than previous.
+    if (this.paginationStart > this.rows.length) {
+      this.paginationStart = this.rows.length - (this.rows.length % this.pageSize);
+    }
     this.parseRows();
     this.setInitialRowProps();
     this.resetSortingToggle();
