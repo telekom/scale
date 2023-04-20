aae96ddb263b6142afa70881a1d5b905d3cd66fb
Marvin_Laubenstein <82942834+marvinLaubenstein@users.noreply.github.com>
2023-04-21 01:35:29 +0200

    refactor: testing

--- packages/components/src/components/search/search.spec.ts
+++ packages/components/src/components/search/search.spec.ts
@@ -18,7 +18,14 @@ describe('Search', () => {
   beforeEach(async () => {
     page = await newSpecPage({
       components: [Search],
-      html: `<scale-search label="test"></scale-search>`,
+      html: ` <scale-search>
+                <scale-icon-action-search slot="prefix-icon"></scale-icon-action-search>
+                <scale-icon-button size="medium" slot="suffix-icon">
+                  <scale-icon-action-microphone
+                    size="24"
+                    accessibility-title="microphone" />
+                </scale-icon-button>
+              </scale-search>`,
     });
   });
 
@@ -30,48 +37,38 @@ describe('Search', () => {
     const specPage = await newSpecPage({
       components: [Search],
       html: `<scale-search
-                  type ="email"
-                  input-mode-type="email"
-                  name ="name"
-                  label ="label"
-                  max-length="2"
-                  min-length="1"
-                  invalid="true"
-                  placeholder ="placeholder"
-                  disabled ="true"
-                  required ="true"
-                  counter ="true" 
-                  value ="value"
-                  input-id ="input-search1"
-                  transparent ="true"
-                  styles ="styles"
-                  list="datalist"
-               ></scale-search>`,
+                name="search_name"
+                invalid="true"
+                max-length="2"
+                min-length="1"
+                placeholder="placeholder"
+                disabled="true"
+                required="true"
+                value="value"
+                input-id="input-search1"
+                transparent="true"
+                aria-detailed-id="id"
+               >
+                <scale-icon-action-search slot="prefix-icon"></scale-icon-action-search>
+                <scale-icon-button size="medium" slot="suffix-icon">
+                  <scale-icon-action-microphone
+                    size="24"
+                    accessibility-title="microphone" />
+                </scale-icon-button
+             ></scale-search>`,
     });
 
-    expect(specPage.rootInstance.type).toBe('email');
-    expect(specPage.rootInstance.inputModeType).toBe('email');
-    expect(specPage.rootInstance.name).toBe('name');
-    expect(specPage.rootInstance.label).toBe('label');
+    expect(specPage.rootInstance.name).toBe('search_name');
+    expect(specPage.rootInstance.invalid).toBe(true);
     expect(specPage.rootInstance.maxLength).toBe(2);
     expect(specPage.rootInstance.minLength).toBe(1);
-    expect(specPage.rootInstance.invalid).toBe(true);
     expect(specPage.rootInstance.placeholder).toBe('placeholder');
     expect(specPage.rootInstance.disabled).toBe(true);
     expect(specPage.rootInstance.required).toBe(true);
-    expect(specPage.rootInstance.counter).toBe(true);
     expect(specPage.rootInstance.value).toBe('value');
-    expect(specPage.rootInstance.transparent).toBe(true);
-    expect(specPage.rootInstance.styles).toBe('styles');
     expect(specPage.rootInstance.inputId).toBe('input-search1');
-    expect(specPage.rootInstance.hasFocus).toBe(false);
-    expect(specPage.rootInstance.list).toBe('datalist');
-  });
-
-  it('should handle helperText prop', async () => {
-    page.root.helperText = 'help';
-    await page.waitForChanges();
-    expect(page.rootInstance.helperText).toBe('help');
+    expect(specPage.rootInstance.transparent).toBe(true);
+    expect(specPage.rootInstance.ariaDetailedId).toBe('id');
   });
 
   it('should handle maxLength prop', async () => {
@@ -84,118 +81,5 @@ describe('Search', () => {
     page.root.minLength = 5;
     await page.waitForChanges();
     expect(page.rootInstance.minLength).toBe(5);
-  });
-
-  it('should emit on input', async () => {
-    const inputSpy = jest.fn();
-    const inputSpyLegacy = jest.fn();
-    page.doc.addEventListener('scale-input', inputSpy);
-    page.doc.addEventListener('scaleInput', inputSpyLegacy);
-    const inputField = page.doc.querySelector('input');
-    inputField.value = TEST_VALUE;
-    await inputField.dispatchEvent(new Event('input'));
-    await page.waitForChanges();
-    expect(inputSpy).toHaveBeenCalled();
-    expect(inputSpyLegacy).toHaveBeenCalled();
-  });
-  it('should react on change event', async () => {
-    // const changeSpy = jest.fn();
-    // page.doc.addEventListener('scaleChange', changeSpy);
-    const inputField = page.doc.querySelector('input');
-    inputField.value = TEST_VALUE;
-    await inputField.dispatchEvent(new Event('change'));
-    await page.waitForChanges();
-    expect(page.rootInstance.value).toBe('Test Value');
-    inputField.value = '';
-    await inputField.dispatchEvent(new Event('change'));
-    await page.waitForChanges();
-    expect(page.rootInstance.value).toBe('');
-    // expect(changeSpy).toHaveBeenCalled();
-  });
-  it('should emit on change', async () => {
-    const emitSpy = jest.fn();
-    const emitSpyLegacy = jest.fn();
-    page.doc.addEventListener('scale-change', emitSpy);
-    page.doc.addEventListener('scaleChange', emitSpyLegacy);
-    page.rootInstance.emitChange();
-    await page.waitForChanges();
-    expect(emitSpy).toHaveBeenCalled();
-    expect(emitSpyLegacy).toHaveBeenCalledTimes(1);
-  });
-  it('should emit on focus', async () => {
-    const focusSpy = jest.fn();
-    const focusSpyLegacy = jest.fn();
-    page.doc.addEventListener('scale-focus', focusSpy);
-    page.doc.addEventListener('scaleFocus', focusSpyLegacy);
-    const inputField = page.doc.querySelector('input');
-    inputField.value = TEST_VALUE;
-    await inputField.dispatchEvent(new Event('focus'));
-    await page.waitForChanges();
-    expect(focusSpy).toHaveBeenCalled();
-    expect(focusSpyLegacy).toHaveBeenCalled();
-  });
-  it('should emit on blur', async () => {
-    const blurSpy = jest.fn();
-    const blurSpyLegacy = jest.fn();
-    page.doc.addEventListener('scale-blur', blurSpy);
-    page.doc.addEventListener('scaleBlur', blurSpyLegacy);
-    const inputField = page.doc.querySelector('input');
-    inputField.value = TEST_VALUE;
-    await inputField.dispatchEvent(new Event('blur'));
-    await page.waitForChanges();
-    expect(blurSpy).toHaveBeenCalled();
-    expect(blurSpyLegacy).toHaveBeenCalled();
-  });
-  it('should emit on keydown', async () => {
-    const keyDownSpy = jest.fn();
-    const keyDownSpyLegacy = jest.fn();
-    page.doc.addEventListener('scaleKeydown', keyDownSpyLegacy);
-    page.doc.addEventListener('scale-keydown', keyDownSpy);
-    const inputField = page.doc.querySelector('input');
-    inputField.value = TEST_VALUE;
-    await inputField.dispatchEvent(new Event('keydown'));
-    await page.waitForChanges();
-    expect(keyDownSpy).toHaveBeenCalled();
-    expect(keyDownSpyLegacy).toHaveBeenCalled();
-  });
-
-  it('should handle css classes', () => {
-    const element = new Search();
-
-    element.hasFocus = true;
-    expect(element.getCssClassMap()).toContain('search--has-focus');
-
-    element.disabled = true;
-    expect(element.getCssClassMap()).toContain('search--disabled');
-
-    element.transparent = true;
-    expect(element.getCssClassMap()).toContain('search--transparent');
-  });
-
-  it('should handle hasFocus right', () => {
-    const element = new Search();
-    element.handleFocus();
-    expect(element.hasFocus).toBeTruthy();
-    element.handleBlur();
-    expect(element.hasFocus).toBeFalsy();
-  });
-
-  it('should emit Onchange only once', async () => {
-    const controlledInputPage = await newSpecPage({
-      components: [Search],
-      html: `<scale-search controlled></scale-search>`,
-    });
-    const inputSpy = jest.fn();
-    const inputSpyLegacy = jest.fn();
-    controlledInputPage.doc.addEventListener('scale-change', inputSpy);
-    controlledInputPage.doc.addEventListener('scaleChange', inputSpyLegacy);
-
-    const inputField = controlledInputPage.doc.querySelector('input');
-
-    inputField.value = TEST_VALUE;
-    await inputField.dispatchEvent(new Event('input'));
-    await controlledInputPage.waitForChanges();
-    expect(inputSpy).toHaveBeenCalledTimes(1);
-    expect(inputSpyLegacy).toHaveBeenCalledTimes(1);
   });
 });
