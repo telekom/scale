7e89687b6ebd5bb63cd4fa60ef9eae669e44e7d8
ayeshaa-m <ayesha.mulla@t-systems.com>
2023-04-21 18:11:59 +0530

    feat: added prop dissmissible and variant

--- packages/components/src/components/search-list-item/search-list-item.tsx
+++ packages/components/src/components/search-list-item/search-list-item.tsx
@@ -1,4 +1,7 @@
-import { Component, h } from '@stencil/core';
+import { Component, Element, h, Host, Prop } from '@stencil/core';
+import classNames from 'classnames';
+
+import { emitEvent } from '../../utils/utils';
 
 @Component({
   tag: 'scale-search-list-item',
@@ -6,26 +9,80 @@ import { Component, h } from '@stencil/core';
   shadow: true,
 })
 export class SearchSelectItem {
+  @Element() hostElement: HTMLElement;
+
+  /** (optional) is close button to be shown */
+  @Prop() dismissible?: boolean = true;
+
+  /** (optional) The buttons to be shown on Hover or always */
+  @Prop() variant?: 'always' | 'hover' = 'hover';
+
+  /**
+   * Handles click event for close button.
+   * @param event click event
+   */
+  handleClick = (event: MouseEvent) => {
+    event.preventDefault();
+    event.stopPropagation();
+    emitEvent(this, 'scaleClose', event);
+  };
+
+  /**
+   * Gets clear button.
+   * @returns clear icon button.
+   */
+  getClearIconButton() {
+    return (
+      <scale-icon-button
+        size="medium"
+        part="clear-icon-button"
+        onClick={this.handleClick}
+      >
+        <scale-icon-action-close
+          part="clear-icon"
+          accessibility-title="close"
+          size={24}
+        />
+      </scale-icon-button>
+    );
+  }
+
   render() {
     return (
-      <div part="base">
-        <div part="prefix">
-          <slot name="prefix"></slot>
-        </div>
-
-        <div part="text">
-          <div part="label">
-            <slot></slot>
+      <Host>
+        <div part="base" tabIndex={0} class={this.getCssClassMap()}>
+          <div part="prefix">
+            <slot name="prefix"></slot>
           </div>
-          <div part="supporting-text">
-            <slot name="supporting-text"></slot>
+
+          <div part="text">
+            <div part="label">
+              <slot></slot>
+            </div>
+            <div part="supporting-text">
+              <slot name="supporting-text"></slot>
+            </div>
+          </div>
+
+          <div
+            part="suffix"
+            // style={{ display: `${this.variant === 'always' ? '' : 'none'}` }}
+            // class={this.getCssClassMap()}
+          >
+            {this.dismissible ? (
+              this.getClearIconButton()
+            ) : (
+              <slot name="suffix"></slot>
+            )}
           </div>
         </div>
-
-        <div part="suffix">
-          <slot name="suffix"></slot>
-        </div>
-      </div>
+      </Host>
     );
+  }
+
+  getCssClassMap() {
+    const component = 'scale-search-list-item';
+
+    return classNames(`${component}-${this.variant}`);
   }
 }
