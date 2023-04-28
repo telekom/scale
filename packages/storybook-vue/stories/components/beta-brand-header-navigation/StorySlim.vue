e9ce0fa9e30e7c4eef85140de7c9239069a14caa
felixw <felix.weber@iconstorm.com>
2023-04-28 12:40:47 +0200

    fix: remove metanav in slim header

--- packages/storybook-vue/stories/components/beta-brand-header-navigation/StorySlim.vue
+++ packages/storybook-vue/stories/components/beta-brand-header-navigation/StorySlim.vue
@@ -9,7 +9,6 @@
     :main-nav-aria-label="mainNavAriaLabel"    
     >
     <SlotMetaNavExternal />
-    <SlotMetaNav />
     <SlotLangSwitcher />
     <SlotMainNavWithFlyout />
     <SlotFunctionsSlim :app-name="appName" :app-name-link="appNameLink" />
@@ -18,7 +17,6 @@
 
 <script>
 import SlotMetaNavExternal from './SlotMetaNavExternal.vue';
-import SlotMetaNav from './SlotMetaNav.vue';
 import SlotMainNavWithFlyout from './SlotMainNavWithFlyout.vue';
 import SlotLangSwitcher from './SlotLangSwitcher.vue';
 import SlotFunctionsSlim from './SlotFunctionsSlim.vue';
@@ -26,7 +24,6 @@ import SlotFunctionsSlim from './SlotFunctionsSlim.vue';
 export default {
   components: {
     SlotMetaNavExternal,
-    SlotMetaNav,
     SlotMainNavWithFlyout,
     SlotLangSwitcher,
     SlotFunctionsSlim,
