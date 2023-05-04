c1a248d2692f02189ae1f8a29a549071004e0557
felixw <felix.weber@iconstorm.com>
2023-05-04 16:03:45 +0200

    docs: add mobile nav story

---
+++ packages/storybook-vue/stories/components/beta-brand-header-navigation/SlotFunctionsMobile.vue
@@ -0,0 +1,35 @@
+<template>
+    <scale-telekom-nav-list
+      variant="functions"
+      slot="functions"
+      alignment="right"
+      aria-label="Functions Menu"
+    >
+      <scale-telekom-nav-item aria-label="Menu">
+        <button>
+          <scale-badge>
+            <scale-icon-action-menu></scale-icon-action-menu>
+          </scale-badge>
+        </button>
+        <scale-telekom-nav-flyout variant="mobile">
+          <SlotMobileFlyoutCanvas
+            :app-name="appName"
+            :app-name-link="appNameLink"
+          />
+        </scale-telekom-nav-flyout>
+      </scale-telekom-nav-item>
+    </scale-telekom-nav-list>
+  </template>
+  
+  <script>
+  import SlotMobileFlyoutCanvas from './SlotMobileFlyoutCanvas.vue';
+  
+  export default {
+    components: { SlotMobileFlyoutCanvas },
+    props: {
+      appName: String,
+      appNameLink: String,
+    },
+  };
+  </script>
+  
---
+++ packages/storybook-vue/stories/components/beta-brand-header-navigation/StoryMobile.vue
@@ -0,0 +1,19 @@
+<template>
+    <scale-telekom-header>
+      <SlotFunctionsMobile/>
+    </scale-telekom-header>
+  </template>
+  
+  <script>
+  import SlotFunctionsMobile from './SlotFunctionsMobile.vue';
+  
+  export default {
+    components: {
+      SlotFunctionsMobile,
+    },
+    props: {
+      appName: String,
+    },
+  };
+  </script>
+  
