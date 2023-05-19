e6fec286b0eb53980de422c410a24b70c1f633b4
Arturo <ac@iconstorm.com>
2023-05-19 16:31:44 +0200

    docs(dropdown-select): adjust stories (a11y)

--- packages/storybook-vue/stories/components/dropdown-select/ScaleDropdownSelect.vue
+++ packages/storybook-vue/stories/components/dropdown-select/ScaleDropdownSelect.vue
@@ -1,25 +1,4 @@
-<template>
-  <scale-dropdown-select
-    :label="label"
-    :helper-text="helperText"
-    :variant="variant"
-    :invalid="invalid"
-    :disabled="disabled"
-    :size="size"
-    :value="value"
-    :combobox-id="comboboxId"
-    @scaleChange="['scale-change']"
-    @scaleFocus="['scale-focus']"
-    @scaleBlur="['scale-blur']"
-    @scaleKeyDown="['scale-keydown']"
-  >
-    <slot></slot>
-  </scale-dropdown-select>
-</template>
-
 <script>
-import { action } from '@storybook/addon-actions';
-
 export default {
   props: {
     label: { type: String },
@@ -31,23 +10,8 @@ export default {
     variant: { type: String },
     comboboxId: { type: String },
   },
-  methods: {
-    'scale-change'($event) {
-      action('scale-change');
-      this.$emit('scale-change', $event);
-    },
-    'scale-focus'($event) {
-      action('scale-focus');
-      this.$emit('scale-focus', $event);
-    },
-    'scale-blur'($event) {
-      action('scale-blur');
-      this.$emit('scale-blur', $event);
-    },
-    'scale-keydown'($event) {
-      action('scale-keydown');
-      this.$emit('scale-keydown', $event);
-    },
-  },
+  render() {
+    return this.$slots.default;
+  }
 };
 </script>
