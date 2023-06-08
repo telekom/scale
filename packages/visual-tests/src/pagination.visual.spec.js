3c7ecfd6c5449b560f86a8a764427e0dfc1be1be
Arturo Castillo Delgado <ac@iconstorm.com>
2023-06-08 10:30:48 +0200

    feat: more visual alignments (#1984)
    * docs(pagination): remove embedded story
    
    * fix(progress-bar): status description font token
    
    * fix(callout): medium size font token
    
    * test(visual): update snapshots (#1985)
    
    Co-authored-by: acstll <acstll@users.noreply.github.com>
    
    * fix(visual-tests): leftover argh
    
    * style: format
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: acstll <acstll@users.noreply.github.com>

--- packages/visual-tests/src/pagination.visual.spec.js
+++ packages/visual-tests/src/pagination.visual.spec.js
@@ -3,27 +3,24 @@ describe('Pagination', () => {
     beforeAll(async () => {
       await global.runColorSetup('components-pagination--standard', mode);
     });
-    test.each([['standard'], ['hidden-borders'], ['embedded-hidden-borders']])(
-      '%p',
-      async (variant) => {
-        await page.goto(
-          `http://host.docker.internal:3123/iframe.html?id=components-pagination--${variant}&viewMode=story`
-        );
-        await page.waitForSelector('html.hydrated');
-        const previewHtml = await page.$('body');
-        await page.evaluate(() => {
-          [
-            '--telekom-motion-duration-immediate',
-            '--telekom-motion-duration-transition',
-            '--telekom-motion-duration-animation',
-            '--telekom-motion-duration-animation-deliberate',
-          ].forEach((transitionSpeed) => {
-            document.body.style.setProperty(transitionSpeed, '0s');
-          });
+    test.each([['standard'], ['hidden-borders']])('%p', async (variant) => {
+      await page.goto(
+        `http://host.docker.internal:3123/iframe.html?id=components-pagination--${variant}&viewMode=story`
+      );
+      await page.waitForSelector('html.hydrated');
+      const previewHtml = await page.$('body');
+      await page.evaluate(() => {
+        [
+          '--telekom-motion-duration-immediate',
+          '--telekom-motion-duration-transition',
+          '--telekom-motion-duration-animation',
+          '--telekom-motion-duration-animation-deliberate',
+        ].forEach((transitionSpeed) => {
+          document.body.style.setProperty(transitionSpeed, '0s');
         });
-        expect(await previewHtml.screenshot()).toMatchImageSnapshot();
-      }
-    );
+      });
+      expect(await previewHtml.screenshot()).toMatchImageSnapshot();
+    });
     test('buttons disabled', async () => {
       await global.runSetup(`components-pagination--standard`);
 
@@ -38,27 +35,24 @@ describe('Pagination', () => {
       await lastButton.click();
       await global.visualCheck();
     });
-    test.each([['hidden-borders'], ['embedded-hidden-borders']])(
-      '%p',
-      async (variant) => {
-        await global.runSetup(`components-pagination--${variant}`);
+    test.each([['hidden-borders']])('%p', async (variant) => {
+      await global.runSetup(`components-pagination--${variant}`);
 
-        const firstButton = await global.page.evaluateHandle(
-          `document.querySelector("#root scale-pagination").shadowRoot.querySelector("div > button.pagination__first-prompt")`
-        );
-        const base = await global.page.evaluateHandle(
-          `document.querySelector("#root")`
-        );
+      const firstButton = await global.page.evaluateHandle(
+        `document.querySelector("#root scale-pagination").shadowRoot.querySelector("div > button.pagination__first-prompt")`
+      );
+      const base = await global.page.evaluateHandle(
+        `document.querySelector("#root")`
+      );
 
-        firstButton.hover();
-        await global.visualCheck();
-        base.hover();
-        firstButton.focus();
-        await global.visualCheck();
-        await global.page.mouse.move(20, 30);
-        await global.page.mouse.down();
-        await global.visualCheck();
-      }
-    );
+      firstButton.hover();
+      await global.visualCheck();
+      base.hover();
+      firstButton.focus();
+      await global.visualCheck();
+      await global.page.mouse.move(20, 30);
+      await global.page.mouse.down();
+      await global.visualCheck();
+    });
   });
 });
