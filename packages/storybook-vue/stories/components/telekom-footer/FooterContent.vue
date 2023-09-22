dc4301e2cb378dec39e7f04389237ae4aec7ea4a
felixw <felix.weber@iconstorm.com>
2023-09-22 10:13:14 +0200

    docs: document new props

---
+++ packages/storybook-vue/stories/components/telekom-footer/FooterContent.vue
@@ -0,0 +1,10 @@
+<script>
+  export default {
+    name: 'FooterContent',
+    props: {
+        logoHref: { type: String, default: 'javascript:void(0);' },
+        logoTitle: { type: String, default: 'Telekom Logo' },
+        logoHideTitle: { type: Boolean, default: false }
+    }
+  };
+</script>
