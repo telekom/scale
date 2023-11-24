9c23883a099c5842b327f2e9b24f67ec9e153b9a
felixw <felix.weber@iconstorm.com>
2023-11-24 14:40:08 +0100

    docs: add new props to storybook

---
+++ packages/storybook-vue/stories/components/tab-navigation/TabPanel.vue
@@ -0,0 +1,17 @@
+<script>
+  export default {
+    name: 'Tab Panel',
+    props: {
+        styles: { type: String },
+        tabbable: { type: Boolean, default: 'true',
+          argTypes:{
+            table: {
+              type: { summary: 'string' },
+            },
+            description: `(optional) Injected styles`,
+            control: { type: null },
+          },
+        },
+      }
+  };
+</script>
