84113e768f54c04e11b3ad008b9e9cf0a21d7a40
Dániel Sulyok <daniel.sulyok@tallence.com>
2023-12-03 20:20:45 +0100

    storybook: add profile menu as separate story

---
+++ packages/storybook-vue/stories/components/telekom-profile-menu/ProfileMenu.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div>{{name}}</div>
+    <div>logged {{loggedIn ? "IN" : "OUT"}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: String,
+    loggedIn: Boolean
+  },
+};
+</script>
---
+++ packages/storybook-vue/stories/components/telekom-profile-menu/profile-menu_de.md
@@ -0,0 +1,6 @@
+<div style="display: inline-flex; align-items: center; justify-content: space-between; width: 100%;">
+    <h1>Profile Menu</h1>
+    <img src="assets/aa.png" alt="Accessible AA" />
+</div>
+
+TODO DaSu (DE)
