34330eabe73688b03af2ebeb7a64b3d9c2b197f2
Dániel Sulyok <daniel.sulyok@tallence.com>
2023-12-13 11:37:06 +0100

    fix: display in storybook

--- packages/storybook-vue/stories/components/telekom-profile-menu/TelekomProfileMenu.vue
+++ packages/storybook-vue/stories/components/telekom-profile-menu/TelekomProfileMenu.vue
@@ -1,35 +1,27 @@
 <template>
-  <div class="wrapper">
-    <scale-telekom-app-shell>
-      <scale-telekom-header slot="header" :app-name="appName">
-        <scale-telekom-nav-list variant="functions" slot="functions" alignment="right" aria-label="Functions Menu">
-          <telekom-profile-menu
-            :app-name="appName"
-            :label="label"
-            :accessibility-label="accessibilityLabel"
-            :close-menu-accessibility-label="closeMenuAccessibilityLabel"
-            :service-name="serviceName"
-            :service-description="serviceDescription"
-            :login-label="loginLabel"
-            :login-help-label="loginHelpLabel"
-            :register-headline="registerHeadline"
-            :register-label="registerLabel"
-            :login-settings-label="loginSettingsLabel"
-            :logout-label="logoutLabel"
-            :logged-in="loggedIn"
-            :user-info="userInfo"
-            :service-links="serviceLinks"
-            :login-url="loginUrl"
-            :login-help-url="loginHelpUrl"
-            :register-url="registerUrl"
-            :login-settings-url="loginSettingsUrl"
-            :hide-login-settings="hideLoginSettings"
-            :logout-url="logoutUrl"
-          ></telekom-profile-menu>
-        </scale-telekom-nav-list>
-      </scale-telekom-header>
-    </scale-telekom-app-shell>
-  </div>
+  <telekom-profile-menu
+    :app-name="appName"
+    :label="label"
+    :accessibility-label="accessibilityLabel"
+    :close-menu-accessibility-label="closeMenuAccessibilityLabel"
+    :service-name="serviceName"
+    :service-description="serviceDescription"
+    :login-label="loginLabel"
+    :login-help-label="loginHelpLabel"
+    :register-headline="registerHeadline"
+    :register-label="registerLabel"
+    :login-settings-label="loginSettingsLabel"
+    :logout-label="logoutLabel"
+    :logged-in="loggedIn"
+    :user-info="userInfo"
+    :service-links="serviceLinks"
+    :login-url="loginUrl"
+    :login-help-url="loginHelpUrl"
+    :register-url="registerUrl"
+    :login-settings-url="loginSettingsUrl"
+    :hide-login-settings="hideLoginSettings"
+    :logout-url="logoutUrl"
+  ></telekom-profile-menu>
 </template>
 
 <script>
@@ -67,14 +59,3 @@ export default {
   },
 };
 </script>
-
-<style>
-.wrapper {
-  margin: -1rem;
-}
-
-scale-telekom-app-shell {
-  --min-height: 200vh;
-  --background: var(--telekom-color-background-surface-subtle);
-}
-</style>
