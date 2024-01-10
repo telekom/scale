739f12551762323bf533e91d11e7f9d72fb6010a
felixw <felix.weber@iconstorm.com>
2024-01-10 15:57:00 +0100

    style: format

--- packages/components/src/components/tab-nav/tab-nav.tsx
+++ packages/components/src/components/tab-nav/tab-nav.tsx
@@ -112,7 +112,9 @@ export class TabNav {
 
   getAllEnabledTabs(): HTMLScaleTabHeaderElement[] {
     return Array.from(
-      this.el.querySelectorAll('scale-tab-header:not([disabled]), scale-tab-header[disabled="false"]')
+      this.el.querySelectorAll(
+        'scale-tab-header:not([disabled]), scale-tab-header[disabled="false"]'
+      )
     );
   }
 
