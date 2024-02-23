dbd1b43d125dda3ca3bfad8932853e29e74e55be
felixw <felix.weber@iconstorm.com>
2024-02-23 16:17:08 +0100

    style: remove console log

--- packages/storybook-vue/.storybook/color-mode-switch-addon/register.js
+++ packages/storybook-vue/.storybook/color-mode-switch-addon/register.js
@@ -20,7 +20,6 @@ const inactiveBtnClassName = 'css-am1h1h';
 const separatorClassName = 'css-14kbt3m';
 
 function switchMode(mode) {
-  console.log('switchMode', document.querySelector('#storybook-preview-iframe'), window.location.href)
   const currentURL = window.location.href;
   // avoid changing the mode for following pages (we use a scale component there and it's not working properly with dark mode)
   if (currentURL.includes('/new-release-sketch-library-update') || currentURL.includes('setup-info-getting-started-for-designers')) {
