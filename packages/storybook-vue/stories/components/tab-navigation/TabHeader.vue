5da657b87655073a43719009ab59e4b1c5ed009f
tshimber <tatiana.shimbereva@telekom.de>
2024-06-13 11:52:40 +0200

    feat: tab header props added to docs

---
+++ packages/storybook-vue/stories/components/tab-navigation/TabHeader.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+  name: 'Tab Header',
+  props: {
+    styles: {type: String},
+    disabled: {type: Boolean, default: 'false'},
+    selected: {type: Boolean, default: 'false'},
+    size: {
+      control: {type: 'select', options: ['large', 'small'], defaultValue: 'small'},
+    },
+  }
+};
+</script>
