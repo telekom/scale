b2617a9629c6caf73f19b4f8e32de42260b61e42
tshimber <tatiana.shimbereva@telekom.de>
2024-06-13 15:03:56 +0200

    fix: prop type fixed

--- packages/storybook-vue/stories/components/tab-navigation/TabHeader.vue
+++ packages/storybook-vue/stories/components/tab-navigation/TabHeader.vue
@@ -5,9 +5,7 @@ export default {
     styles: {type: String},
     disabled: {type: Boolean, default: 'false'},
     selected: {type: Boolean, default: 'false'},
-    size: {
-      control: {type: ['large', 'small'], defaultValue: 'small'}
-    }
+    size: {type: ['large', 'small'], defaultValue: 'small'}
   }
 };
 </script>
