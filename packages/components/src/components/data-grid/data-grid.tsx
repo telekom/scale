093ace2fe1f2fd0d65b42668d9baabc3e0f4b0cb
tshimber <tatiana.shimbereva@telekom.de>
2024-08-08 12:10:46 +0200

    fix: prettier fixes

--- packages/components/src/components/data-grid/data-grid.tsx
+++ packages/components/src/components/data-grid/data-grid.tsx
@@ -183,7 +183,7 @@ export class DataGrid {
     this.fieldsHandler();
     this.rowsHandler();
   }
-  componentWillUpdate() { }
+  componentWillUpdate() {}
   componentDidRender() {
     if (this.needsAutoWidthParse) {
       this.calculateAutoWidths();
@@ -198,7 +198,7 @@ export class DataGrid {
   componentDidLoad() {
     this.addResizeObserver();
   }
-  componentDidUpdate() { }
+  componentDidUpdate() {}
   disconnectedCallback() {
     this.removeResizeObserver();
   }
@@ -290,7 +290,8 @@ export class DataGrid {
       if (this.rows[i].length !== this.fields.length) {
         // tslint:disable-next-line: no-console
         console.warn(
-          `Unable to render ${this.heading && `"${this.heading}" `
+          `Unable to render ${
+            this.heading && `"${this.heading}" `
           }table: row data length not equal to supplied fields.`
         );
         return false;
@@ -409,8 +410,8 @@ export class DataGrid {
       currentSortDirection === 'none'
         ? 'ascending'
         : currentSortDirection === 'ascending'
-          ? 'descending'
-          : 'none';
+        ? 'descending'
+        : 'none';
     this.fields[columnIndex].sortDirection = newSortDirection;
     this.sortTable(newSortDirection, type, columnIndex);
   }
@@ -443,10 +444,10 @@ export class DataGrid {
       });
     } else {
       switch (
-      (CELL_TYPES[type] &&
-        CELL_TYPES[type].defaults &&
-        CELL_TYPES[type].defaults.sortBy) ||
-      CELL_DEFAULTS.sortBy
+        (CELL_TYPES[type] &&
+          CELL_TYPES[type].defaults &&
+          CELL_TYPES[type].defaults.sortBy) ||
+        CELL_DEFAULTS.sortBy
       ) {
         case 'text':
         case 'date':
@@ -490,12 +491,17 @@ export class DataGrid {
   }
 
   presortIfNeeded(): void {
-    const columnToPresort = this.fields.find(col => col.sortable && col.presort);
+    const columnToPresort = this.fields.find(
+      (col) => col.sortable && col.presort
+    );
     if (!columnToPresort) {
       return;
     }
     const columnIndex = this.fields.indexOf(columnToPresort);
-    const direction = columnToPresort.presortDirection === 'descending' ? 'descending' : 'ascending';
+    const direction =
+      columnToPresort.presortDirection === 'descending'
+        ? 'descending'
+        : 'ascending';
     this.presortTable(direction, columnIndex, columnToPresort.type);
   }
 
@@ -1020,11 +1026,11 @@ export class DataGrid {
                   : CELL_DEFAULTS.resizable,
                 width = CELL_TYPES[type].defaults.width || CELL_DEFAULTS.width,
                 minWidth = CELL_TYPES[type].defaults.minWidth ||
-                CELL_DEFAULTS.minWidth,
+                  CELL_DEFAULTS.minWidth,
                 maxWidth = CELL_TYPES[type].defaults.maxWidth ||
-                CELL_DEFAULTS.maxWidth,
+                  CELL_DEFAULTS.maxWidth,
                 textAlign = CELL_TYPES[type].defaults.textAlign ||
-                CELL_DEFAULTS.textAlign,
+                  CELL_DEFAULTS.textAlign,
                 stretchWidth = 0,
               },
               columnIndex
@@ -1049,28 +1055,28 @@ export class DataGrid {
                   {...props}
                   {...(sortable
                     ? {
-                      onKeyDown: (event: KeyboardEvent) => {
-                        if (['Enter', ' '].includes(event.key)) {
-                          this.toggleTableSorting(
-                            sortDirection,
-                            columnIndex,
-                            type
-                          );
-                        }
-                      },
-                      onClick: (e) => {
-                        const clickedElement = e.target as HTMLElement;
-                        if (!clickedElement.matches('.thead__divider')) {
-                          this.toggleTableSorting(
-                            sortDirection,
-                            columnIndex,
-                            type
-                          );
-                        }
-                      },
-                      tabindex: 0,
-                      class: `${props.class} thead-sortable`,
-                    }
+                        onKeyDown: (event: KeyboardEvent) => {
+                          if (['Enter', ' '].includes(event.key)) {
+                            this.toggleTableSorting(
+                              sortDirection,
+                              columnIndex,
+                              type
+                            );
+                          }
+                        },
+                        onClick: (e) => {
+                          const clickedElement = e.target as HTMLElement;
+                          if (!clickedElement.matches('.thead__divider')) {
+                            this.toggleTableSorting(
+                              sortDirection,
+                              columnIndex,
+                              type
+                            );
+                          }
+                        },
+                        tabindex: 0,
+                        class: `${props.class} thead-sortable`,
+                      }
                     : {})}
                 >
                   <div class={`thead__title`}>
@@ -1174,8 +1180,8 @@ export class DataGrid {
                     field.visible !== undefined
                       ? field.visible
                       : CELL_TYPES[field.type].defaults.visible !== undefined
-                        ? CELL_TYPES[field.type].defaults.visible
-                        : CELL_DEFAULTS.visible;
+                      ? CELL_TYPES[field.type].defaults.visible
+                      : CELL_DEFAULTS.visible;
                   if (!visible) {
                     return;
                   }
@@ -1305,8 +1311,9 @@ export class DataGrid {
 
     return (
       <td
-        class={`tbody__cell${mobileTitle ? ` tbody__cell--used-as-mobile-title` : ``
-          }`}
+        class={`tbody__cell${
+          mobileTitle ? ` tbody__cell--used-as-mobile-title` : ``
+        }`}
         style={{ width: `calc(${width}px + ${stretchWidth}px)` }}
       >
         <div class={`tbody__mobile-label`}>{label}</div>
@@ -1326,8 +1333,9 @@ export class DataGrid {
       <div class={`info`}>
         {this.selectable && !!this.selection.length && (
           <div class={`info__selection`}>
-            {`${this.selection.length} row${this.selection.length > 1 ? 's' : ''
-              } selected`}
+            {`${this.selection.length} row${
+              this.selection.length > 1 ? 's' : ''
+            } selected`}
           </div>
         )}
         {this.isPagination && (
